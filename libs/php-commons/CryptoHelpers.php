757c8a13b8c98b86308d0f487a9fe671c89d0aa2
Nathan <nathanws@users.noreply.github.com>
2018-03-08 10:27:31 -0600

    Verify signatures with CryptoHelpers
    Added methods to verify that a signature is authentic for a given
    message and signing key.
    
    Added tests for new methods.

--- libs/php-commons/CryptoHelpers.php
+++ libs/php-commons/CryptoHelpers.php
@@ -2,9 +2,24 @@
 
 /**
  * Suite of helper functions to assist in encrypting and signing data.
+ *
+ * In general, all input and output strings should be in hexadecimal
+ * format, unless otherwise noted.
  */
 class CryptoHelpers
 {
+    /**
+     * Generate a hash for the given object in hexadecimal format.
+     *
+     * @param string    $obj    the object to hash
+     *
+     * @return string   the hash of the object in hexadecimal format
+     */
+    public static function hashObject($obj) : string
+    {
+        return sodium_bin2hex(sodium_crypto_generichash($obj));
+    }
+
     /**
      * Sign a JSON object with the given key.
      *
@@ -21,8 +36,35 @@ class CryptoHelpers
      */
     public static function signJSONObject($json, $hexKey) : string
     {
-        $hashedMessage = sodium_crypto_generichash($json, sodium_hex2bin($hexKey));
+        $hashedMessage = self::hashObject($json);
+        $signature = sodium_crypto_sign_detached(
+            sodium_hex2bin($hashedMessage),
+            sodium_hex2bin($hexKey)
+        );
 
-        return sodium_bin2hex($hashedMessage);
+        return sodium_bin2hex($signature);
+    }
+
+    /**
+     * Verify that the signature for the given message is authentic.
+     *
+     * @param string    $signature  The detached signature to authenticate,
+     *                              in hexadecimal format.
+     * @param string    $msg        The message to authenticate against
+     * @param string    $key        The key to authenticate with, in
+     *                              hexadecimal string format. Usually a
+     *                              public signing key.
+     *
+     * @return bool If the signature is authentic for the given message
+     */
+    public static function verifySignature($signature, $msg, $key) : bool
+    {
+        $verified = sodium_crypto_sign_verify_detached(
+            sodium_hex2bin($signature),
+            sodium_hex2bin(self::hashObject($msg)),
+            sodium_hex2bin($key)
+        );
+
+        return is_bool($verified) && $verified == true;
     }
 }
