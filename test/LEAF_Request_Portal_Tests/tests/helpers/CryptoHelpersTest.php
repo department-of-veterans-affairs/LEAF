a48f914394a3c9f278c9e48101daf1dc6672f84e
Nathan <nathanws@users.noreply.github.com>
2018-03-08 10:27:31 -0600

    Verify signatures with CryptoHelpers
    Added methods to verify that a signature is authentic for a given
    message and signing key.
    
    Added tests for new methods.

--- test/LEAF_Request_Portal_Tests/tests/helpers/CryptoHelpersTest.php
+++ test/LEAF_Request_Portal_Tests/tests/helpers/CryptoHelpersTest.php
@@ -13,6 +13,15 @@ final class CryptoHelpersTest extends DatabaseTest
 {
     private static $portalClient = null;
 
+    // the signing keys, in hexadecimal format
+    private static $secretSignKey = "21f546c87d5b25335bf05ce81b28d662f517aa4520f409c9a76710273a35df167bd9ef2f1049e05ed8abb9fb497bf200e2f9e70451f3ad0e694cf778f001568e";
+    private static $publicSignKey = "7bd9ef2f1049e05ed8abb9fb497bf200e2f9e70451f3ad0e694cf778f001568e";
+
+    // signature of signed object
+    private static $signature = "d134b276d0f858c78b35656e695344346c00e06f71f4eecee2e3f7b0992f2e9cca8db61b117e2bce39d9eb20b467c8c33dbc22972b3cc7791725fcac7c27bb0c";
+
+
+    // runs before every test
     protected function setUp()
     {
         $this->resetDatabase();
@@ -29,23 +38,97 @@ final class CryptoHelpersTest extends DatabaseTest
     }
 
     /**
-     * Tests CryptoHelpers::signJSON()
+     * Tests CryptoHelpers::hashObject()
      */
-    public function testSignJSON() : void
+    public function testHashObject() : void
     {
-        $hexkey = '7847e3dc727339dc7553e80a59ba6431de2aa5d8de6208136371cc220c245c43397e6ee461cdb2f5429d6ac788e3a65813368dcbc2d1ac9bb7b64f3573684f0f';
-        $signature = 'd9c0007d02ebcc2cb24e521ba3e96758fbb04054ebfba7c770f9f93724b0f971';
+        $hexHash = "5940716038aa9c53f1fb6fc66b185b04d6d71222db10c1440c609576fa85b0c5";
 
         $formToSign = self::$portalClient->get('?a=form/1/dataforsigning');
         $formObj = json_encode($formToSign, JSON_FORCE_OBJECT);
-        $sig = CryptoHelpers::signJSONObject($formObj, $hexkey);
 
-        $this->assertEquals($signature, $sig);
+        $hashed = CryptoHelpers::hashObject($formObj);
+
+        $this->assertEquals($hexHash, $hashed);
+    }
+
+    /**
+     * Tests CryptoHelpers::signJSONObject()
+     */
+    public function testSignJSONObject() : void
+    {
+        $formToSign = self::$portalClient->get('?a=form/1/dataforsigning');
+        $formObj = json_encode($formToSign, JSON_FORCE_OBJECT);
+        $sig = CryptoHelpers::signJSONObject($formObj, self::$secretSignKey);
+
+        $this->assertEquals(self::$signature, $sig);
 
         $anotherFormToSign = self::$portalClient->get('?a=form/2/dataforsigning');
         $anotherFormObj = json_encode($anotherFormToSign, JSON_FORCE_OBJECT);
-        $anotherSig = CryptoHelpers::signJSONObject($anotherFormObj, $hexkey);
+        $anotherSig = CryptoHelpers::signJSONObject($anotherFormObj, self::$secretSignKey);
 
         $this->assertTrue($sig != $anotherSig);
+    }
+
+    /**
+     * Tests CryptoHelpers::verifySignature()
+     * 
+     * Tests an authentic signature.
+     */
+    public function testVerifySignature_authentic() : void
+    {
+        $formToSign = self::$portalClient->get('?a=form/1/dataforsigning');
+        $formObj = json_encode($formToSign, JSON_FORCE_OBJECT);
+
+        $verified = CryptoHelpers::verifySignature(
+            self::$signature,
+            $formObj,
+            self::$publicSignKey
+        );
+
+        $this->assertEquals(true, $verified);
+    }
+
+    /**
+     * Tests CryptoHelpers::verifySignature()
+     * 
+     * Tests an inauthentic signature.
+     */
+    public function testVerifySignature_inauthentic(): void
+    {
+        $differentSignature = CryptoHelpers::signJSONObject(
+            "somerandommjunkdatacanbeanythingdoesntneedtobejson", 
+            self::$secretSignKey
+        );
+
+        $formToSign = self::$portalClient->get('?a=form/1/dataforsigning');
+        $formObj = json_encode($formToSign, JSON_FORCE_OBJECT);
+        $verified = CryptoHelpers::verifySignature(
+            $differentSignature,
+            $formObj,
+            self::$publicSignKey
+        );
+
+        $this->assertEquals(false, $verified);
+    }
+
+    /**
+     * Tests CryptoHelpers::verifySignature()
+     * 
+     * Tests that an authentic signature does not authenticate against
+     * a different message than it was generated for.
+     */
+    public function testVerifySignature_incorrectMessage(): void
+    {
+        $formToSign = self::$portalClient->get('?a=form/2/dataforsigning');
+        $formObj = json_encode($formToSign, JSON_FORCE_OBJECT);
+
+        $verified = CryptoHelpers::verifySignature(
+            self::$signature,
+            $formObj,
+            self::$publicSignKey
+        );
+
+        $this->assertEquals(false, $verified);
     }
 }
