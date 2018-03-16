a1c596ad3af88551569e44240ca6474c203eb615
Nathan <nathanws@users.noreply.github.com>
2018-03-16 11:50:21 -0500

    Sanitizing API inputs
    Sanitized several API inputs for the Request Portal.

--- LEAF_Request_Portal/FormWorkflow.php
+++ LEAF_Request_Portal/FormWorkflow.php
@@ -4,7 +4,7 @@
     Date Created: May 25, 2011
 */
 
-require_once '../libs/php-commons/XSSHelpers.php';
+require_once dirname(__FILE__) . '/../libs/php-commons/XSSHelpers.php';
 
 class FormWorkflow
 {
@@ -377,7 +377,7 @@ class FormWorkflow
         if($_POST['CSRFToken'] != $_SESSION['CSRFToken']) {
             return array('status' => 0, 'errors' => ['Invalid Token']);
         }
-        $comment = $this->sanitizeInput($comment);
+        $comment = XSSHelpers::sanitzeHTML($comment);
         $time = time();
 
         // first check if the user has access
@@ -927,7 +927,7 @@ class FormWorkflow
 	 */
 	public function setStep($stepID, $bypassAdmin = false, $comment = "")
 	{
-		$comment = $this->sanitizeInput($comment);
+		$comment = XSSHelpers::sanitizeHTML($comment);
 
 		if($this->recordID == 0
 			|| (!$this->login->checkGroup(1) && $bypassAdmin == false)) {
@@ -980,81 +980,6 @@ class FormWorkflow
 		return true;
 	}
 
-    /**
-    * Clean up html input, allow some tags
-    * @param string $in
-    * @return string
-    */
-    public function sanitizeInput($in)
-    {
-        // strip out uncommon characters
-        $in = preg_replace('/[^\040-\176]/', '', $in);
-
-        // hard character limit of 65535
-        $in = strlen($in) > 65535 ? substr($in, 0, 65535) : $in;
-    
-        $pattern = array('/&lt;table(\s.+)?&gt;/Ui',
-                             '/&lt;\/table&gt;/Ui',
-                             '/&lt;(\/)?br(\s.+)?\s\/&gt;/Ui',
-                             '/&lt;(\/)?(\S+)(\s.+)?&gt;/U',
-                             '/\b\d{3}-\d{2}-\d{4}\b/', // mask SSN
-                             '/(\<\/p\>\<\/p\>){2,}/',
-                             '/(\<p\>\<\/p\>){2,}/');
-    
-        $replace = array('<table class="table">',
-                             '</table>',
-                             '<\1br />',
-                             '<\1\2>',
-                             '###-##-####',
-                             '',
-                             '');
-    
-        $in = strip_tags(html_entity_decode($in), '<b><i><u><ol><li><br><p><table><td><tr>');
-        $in = preg_replace($pattern, $replace, htmlspecialchars($in, ENT_QUOTES));
-    
-        // verify tag grammar
-        $matches = array();
-        preg_match_all('/\<(\/)?([A-Za-z]+)(\s.+)?\>/U', $in, $matches, PREG_PATTERN_ORDER);
-        $openTags = array();
-        $numTags = count($matches[2]);
-        for($i = 0; $i < $numTags; $i++) {
-            if($matches[2][$i] != 'br') {
-                //echo "examining: {$matches[1][$i]}{$matches[2][$i]}\n";
-                // proper closure
-                if($matches[1][$i] == '/' && isset($openTags[$matches[2][$i]]) && $openTags[$matches[2][$i]] > 0) {
-                    $openTags[$matches[2][$i]]--;
-                    // echo "proper\n";
-                }
-                // new open tag
-                else if($matches[1][$i] == '') {
-                    if(!isset($openTags[$matches[2][$i]])) {
-                        $openTags[$matches[2][$i]] = 0;
-                    }
-                    $openTags[$matches[2][$i]]++;
-                    // echo "open\n";
-                }
-                // improper closure
-                else if($matches[1][$i] == '/' && isset($openTags[$matches[2][$i]]) && $openTags[$matches[2][$i]] <= 0) {
-                    $in = '<' . $matches[2][$i] . '>' . $in;
-                    $openTags[$matches[2][$i]]--;
-                    // echo "improper\n";
-                }
-                // print_r($openTags);
-            }
-        }
-    
-        // close tags
-        $tags = array_keys($openTags);
-        foreach($tags as $tag) {
-            while($openTags[$tag] > 0) {
-                $in = $in . '</' . $tag . '>';
-                $openTags[$tag]--;
-            }
-        }
-    
-        return $in;
-    }
-    
     public function setEventFolder($folder)
     {
         $this->eventFolder = $folder;
