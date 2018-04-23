cd5a199a62fa2910b49ed8d03c3d329a32a4e555
mgaoVA <Michael.Gao@va.gov>
2018-04-23 17:16:02 -0400

    show correct name
    clarify template

--- LEAF_Request_Portal/scripts/custom_js/TEMPLATE_workflow_YOURNAME.js
+++ LEAF_Request_Portal/scripts/custom_js/TEMPLATE_workflow_YOURNAME.js
@@ -1,6 +1,6 @@
 var workflowModule_[dependencyID] = (function() {
 	function init() {
-		$('#form_dep_extension1').html('<div style="padding: 4px">\
+		$('#form_dep_extension' + dependencyID).html('<div style="padding: 4px">\
 				<div style="background-color: white; border: 1px solid black; padding: 4px">test</div>\
 				</div>');		
 	}
