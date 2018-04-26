3f1099262d20a4586a29e2881227b4e011687c88
Nathan <nathanws@users.noreply.github.com>
2018-04-26 13:02:41 -0500

    Portal FormEditor javascript API
    Added FormEditor javascript API module.

--- libs/jsapi/portal/LEAFPortalAPI.js
+++ libs/jsapi/portal/LEAFPortalAPI.js
@@ -4,6 +4,12 @@
 var LEAFRequestPortalAPI = function () {
     var baseURL = './api/?a=',
         Forms = PortalFormsAPI(baseURL),
+        FormEditor = PortalFormEditorAPI(baseURL),
+        Signature = PortalSignaturesAPI(baseURL),
+        Workflow = PortalWorkflowAPI(baseURL),
+
+        // used for POST requests
+        csrfToken = '',
 
         /**
          * Get the base URL for the LEAF Portal API (e.g. "/LEAF_Request_Portal/api/?a=")
@@ -19,15 +25,29 @@ var LEAFRequestPortalAPI = function () {
          * 
          * @param baseURL   string  base URL
          */
-        setBaseURL = function (baseURL) {
-            this.baseURL = baseURL;
+        setBaseURL = function (urlBase) {
+            baseURL = urlBase;
+            Forms.setBaseAPIURL(baseURL);
+            FormEditor.setBaseAPIURL(baseURL);
+            Signature.setBaseAPIURL(baseURL);
+            Workflow.setBaseAPIURL(baseURL);
+        },
+
+        setCSRFToken = function (token) {
+            csrfToken = token;
+            FormEditor.setCSRFToken(token);
+            Signature.setCSRFToken(token);
+            Workflow.setCSRFToken(token);
         };
 
     return {
         getBaseURL: getBaseURL,
         setBaseURL: setBaseURL,
 
-        Forms: Forms
+        Forms: Forms,
+        FormEditor: FormEditor,
+        Signature: Signature,
+        Workflow: Workflow
     };
 };
 
@@ -84,5 +104,261 @@ var PortalFormsAPI = function (baseAPIURL) {
         getAPIURL: getAPIURL,
         getBaseAPIURL: getBaseAPIURL,
         query: query
+    };
+};
+
+var PortalFormEditorAPI = function (baseAPIURL) {
+    var apiBaseURL = baseAPIURL,
+        apiURL = baseAPIURL + 'formEditor',
+
+        // used for POST requests
+        csrfToken = '',
+
+        /**
+         * Get the URL for the LEAF Portal FormEditor API
+         */
+        getAPIURL = function () {
+            return apiURL;
+        },
+
+        /**
+         * Get the base URL for the LEAF Portal API
+         * 
+         * @return string   the base LEAF Portal API URL used in this FormEditor API
+         */
+        getBaseAPIURL = function () {
+            return apiBaseURL;
+        },
+
+        /**
+         * Set the base URL for the LEAF Portal API
+         * 
+         * @param baseAPIURL string the base URL for the Portal API
+         */
+        setBaseAPIURL = function (baseAPIURL) {
+            apiBaseURL = baseAPIURL;
+            apiURL = baseAPIURL + 'formEditor';
+        },
+
+        /**
+         * Set the CSRFToken for POST requests
+         */
+        setCSRFToken = function (token) {
+            csrfToken = token;
+        },
+
+        /**
+         * Get the access privileges for the given indicator
+         * 
+         * @param indicatorID   int                     the id of the indicator to retrieve privileges for
+         * @param onSuccess     function(array[int])    callback containing an array of group IDs
+         * @param onFail        function(err)           callback when action fails
+         */
+        getIndicatorPrivileges = function (indicatorID, onSuccess, onFail) {
+            var fetchURL = apiURL + '/indicator/' + indicatorID + '/privileges';
+
+            $.ajax({
+                method: 'GET',
+                url: fetchURL,
+                dataType: 'json'
+            })
+                .done(function (msg) {
+                    onSuccess(msg);
+                })
+                .fail(function (err) {
+                    onFail(err);
+                });
+            // .always(function() {});
+
+        },
+
+        /**
+         * Set the access privileges for the given indicator
+         * 
+         * @param indicatorID   int                 the id of the indicator to set privileges for
+         * @param groupIDs      array[int]          an array containing the IDs of the groups that should have access
+         * @param onSuccess     function(success)   callback containing true/false if action succeeded
+         * @param onFail        function(err)       callback when action fails
+         */
+        setIndicatorPrivileges = function (indicatorID, groupIDs, onSuccess, onFail) {
+
+            $.ajax({
+                method: 'POST',
+                url: apiURL + '/indicator/' + indicatorID + '/privileges',
+                dataType: "text",
+                data: {
+                    "groupIDs": groupIDs,
+                    CSRFToken: csrfToken
+                }
+            })
+                .done(function (msg) {
+                    onSuccess(msg);
+                })
+                .fail(function (err) {
+                    onFail(err);
+                });
+        };
+
+    return {
+        getAPIURL: getAPIURL,
+        getBaseAPIURL: getBaseAPIURL,
+        setBaseAPIURL: setBaseAPIURL,
+        setCSRFToken: setCSRFToken,
+        getIndicatorPrivileges: getIndicatorPrivileges,
+        setIndicatorPrivileges: setIndicatorPrivileges
+    };
+};
+
+var PortalSignaturesAPI = function (baseAPIURL) {
+    var apiBaseURL = baseAPIURL,
+        apiURL = baseAPIURL + 'signature',
+
+        // used for POST requests
+        csrfToken = '',
+
+        /**
+         * Get the URL for the LEAF Portal Signatures API
+         */
+        getAPIURL = function () {
+            return apiURL;
+        },
+
+        /**
+         * Get the base URL for the LEAF Portal API
+         * 
+         * @return string   the base LEAF Portal API URL used in this Forms API
+         */
+        getBaseAPIURL = function () {
+            return apiBaseURL;
+        },
+
+        /**
+         * Set the base URL for the LEAF Portal API
+         * 
+         * @param baseAPIURL string the base URL for the Portal API
+         */
+        setBaseAPIURL = function (baseAPIURL) {
+            apiBaseURL = baseAPIURL;
+            apiURL = baseAPIURL + 'signature';
+        },
+
+        /**
+         * Set the CSRFToken for POST requests
+         */
+        setCSRFToken = function (token) {
+            csrfToken = token;
+        },
+
+        /**
+         * Create a signature
+         * 
+         * @param signature string          the signature footprint
+         * @param recordID  int             the id of the Record the Signature is associated with
+         * @param message   string          the message that was signed
+         * @param onSuccess function(id)    callback containing the id of the new signature
+         * @param onFail    function(err)   callback when operation fails
+         */
+        create = function (signature, recordID, message, onSuccess, onFail) {
+            $.ajax({
+                method: 'POST',
+                url: apiURL + '/create',
+                dataType: "text",
+                data: {
+                    "signature": signature,
+                    "recordID": recordID,
+                    "message": message,
+                    CSRFToken: csrfToken
+                }
+            })
+                .done(function (msg) {
+                    onSuccess(msg);
+                })
+                .fail(function (err) {
+                    onFail(err);
+                });
+
+        };
+
+    return {
+        getAPIURL: getAPIURL,
+        getBaseAPIURL: getBaseAPIURL,
+        setBaseAPIURL: setBaseAPIURL,
+        setCSRFToken: setCSRFToken,
+        create: create
+    };
+};
+
+/**
+ * API for working with Workflows
+ * 
+ * @param baseAPIURL string the base URL for the LEAF Portal API (e.g. "/LEAF_Request_Portal/api/?a=")
+ */
+var PortalWorkflowAPI = function (baseAPIURL) {
+    var apiBaseURL = baseAPIURL,
+        apiURL = baseAPIURL + 'workflow',
+
+        // used for POST requests
+        csrfToken = '',
+
+        /**
+         * Get the URL for the LEAF Portal Workflow API
+         */
+        getAPIURL = function () {
+            return apiURL;
+        },
+
+        /**
+         * Get the base URL for the LEAF Portal API
+         * 
+         * @return string   the base LEAF Portal API URL used in this Forms API
+         */
+        getBaseAPIURL = function () {
+            return apiBaseURL;
+        },
+
+        /**
+         * Set the base URL for the LEAF Portal API
+         * 
+         * @param baseAPIURL string the base URL for the Portal API
+         */
+        setBaseAPIURL = function (baseAPIURL) {
+            apiBaseURL = baseAPIURL;
+            apiURL = baseAPIURL + 'workflow';
+        },
+
+        setCSRFToken = function (token) {
+            csrfToken = token;
+        },
+
+        /**
+         * Set whether a Step in the specified Workflow requires a Digital Signature
+         * 
+         * @param workflowID            int                 the Workflow ID
+         * @param stepID                int                 the Step ID
+         * @param requiresSignature     boolean             whether a Digital Signature is required
+         * @param onSuccess             function(result)    callback when operation succeeds
+         * @param onFail                function(error)     callback when operation fails
+         */
+        setStepSignatureRequirement = function (workflowID, stepID, requiresSignature, onSuccess, onFail) {
+            $.ajax({
+                method: 'POST',
+                url: apiURL + '/' + workflowID + '/step/' + stepID + '/requiresig',
+                dataType: "text",
+                data: { "requiresSig": requiresSignature, CSRFToken: csrfToken }
+            })
+                .done(function (msg) {
+                    onSuccess(msg);
+                })
+                .fail(function (err) {
+                    onFail(err);
+                });
+        };
+
+    return {
+        getAPIURL: getAPIURL,
+        getBaseAPIURL: getBaseAPIURL,
+        setBaseAPIURL: setBaseAPIURL,
+        setCSRFToken: setCSRFToken,
+        setStepSignatureRequirement: setStepSignatureRequirement
     };
 };
