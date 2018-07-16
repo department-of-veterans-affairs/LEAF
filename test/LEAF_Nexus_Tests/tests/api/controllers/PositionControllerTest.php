33f3cb28c0743833b0c342ee651dbef399d3cb28
jeanmarcablevets <39602732+jeanmarcablevets@users.noreply.github.com>
2018-07-16 17:11:35 -0400

    updated test/remove employee and adjusted comments

--- test/LEAF_Nexus_Tests/tests/api/controllers/PositionControllerTest.php
+++ test/LEAF_Nexus_Tests/tests/api/controllers/PositionControllerTest.php
@@ -22,17 +22,17 @@ class PositionControllerTest extends DatabaseTest
      */
     public function testSetSupervisor() : void
     {
+        //checks initial value
         $results = self::$client->get('position/3/supervisor');
         $this->assertEquals("Medical Center Director", $results[0]["positionTitle"]);
-        //initial value
 
+        //changes supervisor from med center director to test position super
         $newSupervisor = array("positionID" => "2");
         self::$client->postEncodedForm('position/3/supervisor', $newSupervisor);
-        //changes supervisor from med center director to test position super
 
+        //checks that the change was successful
         $results = self::$client->get('position/3/supervisor');
         $this->assertEquals("Test Position Title Super", $results[0]["positionTitle"]);
-        //checks that the change was successful
     }
 
     /**
@@ -40,17 +40,17 @@ class PositionControllerTest extends DatabaseTest
      */
     public function testDeletePosition(): void
     {
+        //checks initial value
         $results = self::$client->get('position/3');
         $this->assertEquals("Test Subordinate Position", $results['title']);
-        //initial value
 
-        self::$client->delete('position/3');
         //deletes position
+        self::$client->delete('position/3');
 
-        $results = self::$client->get('position/3');
-        $this->assertFalse($results['title']);
         //checks to make sure position has been deleted
         //will be false if deleted
+        $results = self::$client->get('position/3');
+        $this->assertFalse($results['title']);
     }
 
     /**
@@ -58,17 +58,17 @@ class PositionControllerTest extends DatabaseTest
      */
     public function testPositionEditTitle() : void
     {
+        //checks initial value
         $results = self::$client->get('position/3');
         $this->assertEquals("Test Subordinate Position", $results['title']);
-        //initial value
 
+        //changes position title
         $newPositionTitle = array('title' => 'anotherNewTitle');
         self::$client->postEncodedForm('position/3/title', $newPositionTitle);
-        //changes position title
 
+        //checks to make sure the change was successful
         $results = self::$client->get('position/3');
         $this->assertEquals("anotherNewTitle", $results['title']);
-        //checks to make sure the change was successful
     }
 
     /**
@@ -76,23 +76,23 @@ class PositionControllerTest extends DatabaseTest
      */
     public function testAddAndRemoveEmployee() : void
     {
+        //checks initial value
         $results = self::$client->get('position/3/employees');
-        $this->assertNull($results[0]);
-        //initial value
+        $this->assertEquals(0, count($results));
 
+        //adds tester employee to test position sub
         $employee = array('empUID' => '1', "isActing" => '0');
         self::$client->postEncodedForm('position/3/employee', $employee);
-        //adds tester employee to test position sub
 
+        //checks to make sure the change was successful and tester is in the position
         $results = self::$client->get('position/3/employees');
         $this->assertEquals('1', $results[0]['empUID']);
-        //checks to make sure the change was successful and tester is in the position
 
-        self::$client->delete('position/3/employee/1');
         //deletes tester from position
+        self::$client->delete('position/3/employee/1');
 
-        $results = self::$client->get('position/3/employees');
-        $this->assertNull($results[0]);
         //checks to make sure change was successful
+        $results = self::$client->get('position/3/employees');
+        $this->assertEquals(0, count($results));
     }
 }
