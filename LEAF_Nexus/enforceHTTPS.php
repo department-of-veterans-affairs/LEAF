e79cb4f731cb6a737feffd605559df5225b8d42d
jeremyrl <41297510+jeremyrl@users.noreply.github.com>
2018-08-08 12:22:45 -0500

    added scubbers to header redirects

--- LEAF_Nexus/enforceHTTPS.php
+++ LEAF_Nexus/enforceHTTPS.php
@@ -3,12 +3,15 @@
  * As a work of the United States government, this project is in the public domain within the United States.
  */
 
+// Include XSSHelpers
+include_once dirname(__FILE__) . '/../libs/php-commons/XSSHelpers.php';
+
 // Enforce HTTPS
 if (isset($config->enforceHTTPS) && $config->enforceHTTPS == true)
 {
     if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on')
     {
-        header('Location: https://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']);
+        header('Location: https://' . XSSHelpers::scrubNewLinesFromURL($_SERVER['SERVER_NAME']) . XSSHelpers::scrubNewLinesFromURL($_SERVER['REQUEST_URI']));
         exit();
     }
 }
