748f24ed94a7e46051a60c53748b72cd88163a36
jeremyrl <41297510+jeremyrl@users.noreply.github.com>
2018-08-15 09:27:39 -0500

    added_testing_endpoints
    Added controllers and endpoints in the tests directory

--- test/LEAF_Request_Portal_Tests/tests/api/SystemControllerTest.php
+++ test/LEAF_Request_Portal_Tests/tests/api/SystemControllerTest.php
@@ -7,9 +7,6 @@ declare(strict_types = 1);
 
 use LEAFTest\LEAFClient;
 
-include '../../LEAF_Request_Portal/db_mysql.php';
-include '../../LEAF_Request_Portal/db_config.php';
-
 /**
  * Tests LEAF_Request_Portal/api/?a=system API
  */
