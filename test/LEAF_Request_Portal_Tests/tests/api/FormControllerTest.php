bedecd1c10e7eb0e13a74560a1c31d72e7222b31
jeanmarcablevets <jeanmarc.daviau@ablevets.com>
2018-08-23 10:02:59 -0400

    Added tests

--- test/LEAF_Request_Portal_Tests/tests/api/FormControllerTest.php
+++ test/LEAF_Request_Portal_Tests/tests/api/FormControllerTest.php
@@ -7,7 +7,7 @@ use LEAFTest\LEAFClient;
 /**
  * Tests LEAF_Request_Portal/api/?a=form API
  */
-final class FormTest extends DatabaseTest
+final class FormControllerTest extends DatabaseTest
 {
     private static $reqClient = null;
 
@@ -88,4 +88,90 @@ final class FormTest extends DatabaseTest
         $this->assertEquals('05/23/1934', $ind6['value']);
     }
 
+    /**
+     * Tests the `form/[digit]` endpoint.
+     */
+    public function testGetForm() : void
+    {
+        $results = self::$reqClient->get('?a=form/1');
+
+        $this->assertNotNull($results);
+        $this->assertNotNull($results['items']);
+        $this->assertEquals(1, count($results['items']));
+
+        $form = $results['items'][0];
+
+        $this->assertEquals('Sample Form', $form['name']);
+
+        $this->assertNotNull($form['children']);
+        $this->assertEquals(7, count($form['children']));
+        $this->assertEquals('form_f4687', $form['children'][0]['type']);
+    }
+
+    /**
+     * Tests the `form/signatures/[digit]` endpoint.
+     */
+    public function testGetFormSignatures() : void
+    {
+        $results = self::$reqClient->get('?a=form/signatures/1');
+
+        $this->assertNotNull($results);
+        $this->assertEquals('test@email.com', $results[0][0]['data']);
+        $this->assertEquals('tester', $results[0][0]['firstName']);
+        $this->assertEquals('tester', $results[0][0]['lastName']);
+        $this->assertEquals('1520268931', $results[0]['time']);
+        $this->assertEquals('1', $results[0]['signature_id']);
+    }
+
+    /**
+     * Tests the `form/[digit]/[digit]/[digit]/history` endpoint.
+     */
+    public function testGetIndicatorLog() : void
+    {
+        $results = self::$reqClient->get('?a=form/1/2/1/history');
+
+        $this->assertNotNull($results);
+        $this->assertEquals(1, count($results));
+        $this->assertEquals('Bruce', $results[0]['data']);
+    }
+
+    /**
+     * Tests the `form/[text]/workflow` endpoint.
+     */
+    public function testGetWorkflow() : void
+    {
+        $results = self::$reqClient->get('?a=form/_form_f4687/workflow');
+
+        $this->assertNotNull($results);
+        $this->assertEquals(1, count($results));
+        $this->assertEquals(1, $results[0]['workflowID']);
+        $this->assertEquals('form_f4687', $results[0]['categoryID']);
+    }
+
+    /**
+     * Tests the `form/[text]/workflow` endpoint.
+     * 
+     * Tests with invalid category ID
+     */
+    public function testGetWorkflow_invalidCategory() : void
+    {
+        $results = self::$reqClient->get('?a=form/_form_junk/workflow');
+
+        $this->assertNotNull($results);
+        $this->assertEquals(0, count($results));
+    }
+
+    /**
+     * Tests the `form/new` endpoint.
+     */
+    public function testNewForm() : void
+    {
+        $results = self::$reqClient->postEncodedForm('?a=form/new', array(
+            'title' => "Junk Title",
+            'numform_f4687' => 1
+        ));
+
+        $this->assertNotNull($results);
+        $this->assertEquals(2, $results);
+    }
 }
