8c0855ff9480517ae170e34e3105bc3a4651ac66
jeanmarcablevets <jeanmarc.daviau@ablevets.com>
2018-09-26 16:34:54 -0400

    Created $leafSecure in Config files, forced PIV login when enabled
    # Conflicts:
    #	LEAF_Nexus/auth_domain/index.php
    #	LEAF_Nexus/config-example.php
    #	LEAF_Nexus/login/index.php
    #	LEAF_Request_Portal/auth_domain/index.php
    #	LEAF_Request_Portal/db_config-example.php
    #	LEAF_Request_Portal/login/index.php

--- LEAF_Request_Portal/auth_domain/index.php
+++ LEAF_Request_Portal/auth_domain/index.php
@@ -21,7 +21,7 @@ $db_phonebook = new DB($config->phonedbHost, $config->phonedbUser, $config->phon
 
 $login = new Login($db_phonebook, $db);
 
-if (isset($_SERVER['REMOTE_USER']))
+if (isset($_SERVER['REMOTE_USER']) && Config::$leafSecure == false)
 {
     $protocol = 'http://';
     if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on')
