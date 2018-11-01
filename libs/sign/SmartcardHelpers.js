cf001ca006f83c61d8d6037155ee48f6d8fab36f
mgaoVA <Michael.Gao@va.gov>
2018-11-01 13:57:17 -0400

    Cleanup

--- libs/sign/SmartcardHelpers.js
+++ libs/sign/SmartcardHelpers.js
@@ -4,7 +4,6 @@ var Signer = function() {
     var initiatedJNLP = false;
     var socket = null;
     function connect(_callback) {
-        console.log('begin connect, initiatedJNLP = ' + initiatedJNLP + ", socket = " + socket);
         if(socket != null
             && socket.readyState == 1) {
             if(typeof _callback == 'function') {
@@ -12,21 +11,17 @@ var Signer = function() {
             }
             return 1;
         }
-        //socket = new WebSocket('ws://localhost:8443');
+
         var chromeUrl = 'ws://localhost:8080/';
         var ieUrl = 'https://localhost:8443/myapp/';
         !!document.documentMode ? socket = new SockJS(ieUrl) : socket = new WebSocket(chromeUrl);
         socket.addEventListener('open', function() {
-            console.log('onopen');
             isConnected = true;
             if(typeof _callback == 'function') {
                 _callback();
             }
         });
         socket.addEventListener('close', function() {
-            console.log('close.  initiatedJNLP = ' + initiatedJNLP);
-            //if(socket.readyState == 3 || socket.readyState == 0) { // closed/can't open
-            console.log("Connection Closed/Can't Open - Trying to reconnect. ReadyState: " + socket.readyState);
             setTimeout(function() {
                 connect(_callback);
             }, 500);
@@ -36,7 +31,6 @@ var Signer = function() {
                     window.open("//" + window.location.hostname + "/LEAF/digital-signature/sign.jnlp");
                 }
             }
-            // }
         });
         socket.addEventListener('message', function(e) {
             var response = JSON.parse(e.data);
@@ -51,17 +45,13 @@ var Signer = function() {
                         + '<br />Please refresh the page and try again.';
                     break;
                 default:
-                    console.log('Full response: ');
-                    console.log(response);
                     break;
             }
         });
     }
     function sendData(key, dataToSign) {
-        console.log('sendData begin');
         socket.send(JSON.stringify({'key': key,
             'dataToSign': dataToSign}));
-        console.log('sendData end');
     }
     var sign = function (key, dataToSign, onSuccess) {
         connect(function() {
