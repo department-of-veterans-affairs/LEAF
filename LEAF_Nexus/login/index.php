2c796974c689ed3ae478a41529f5cac05d963bbb
jeanmarcablevets <jeanmarc.daviau@ablevets.com>
2018-11-06 15:04:45 -0500

    Added check to see if variable exists

--- LEAF_Nexus/login/index.php
+++ LEAF_Nexus/login/index.php
@@ -60,7 +60,7 @@ When logging into this system, you agree to the following:<br />
     This information system is provided for U.S. Government-authorized use only. Unauthorized or improper use of this system may result in disciplinary action, as well as civil and criminal penalties.<br /><br />
 
     <a href="<?php echo '//' . $_SERVER['SERVER_NAME'] . ':444' . getBaseDir() . 'auth_token/?' . htmlentities($_SERVER['QUERY_STRING'], ENT_QUOTES | ENT_HTML5, 'UTF-8'); ?>"><div class="buttonNorm" style="text-align: center">Login with your VA PIV card<img src="../../libs/dynicons/?img=go-next.svg&amp;w=32" alt="Icon for Login" title="Icon for Login" /></div></a><br />
-    <?php if (Orgchart\Config::$leafSecure == false)
+    <?php if (!isset(Orgchart\Config::$leafSecure) || Orgchart\Config::$leafSecure == false)
     {
         echo '<a href="//' . $_SERVER['SERVER_NAME'] . getBaseDir() . 'auth_domain/?' . htmlentities($_SERVER['QUERY_STRING'], ENT_QUOTES | ENT_HTML5, 'UTF-8') . '"><div class="buttonNorm" style="text-align: center">Login with Username and Password</div></a>';
     }
