e835f49b5f0a28b138a7cb7b4a1f31fadae87fe6
mgaoVA <Michael.Gao@va.gov>
2018-11-15 15:24:37 -0500

    fix

--- LEAF_Request_Portal/utils/db_upgrade/Update_RMC_DB_5360-5366.sql
+++ LEAF_Request_Portal/utils/db_upgrade/Update_RMC_DB_5360-5366.sql
@@ -6,10 +6,13 @@ CREATE TABLE `short_links` (
   `hash` varchar(64) NOT NULL,
   `data` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-ALTER TABLE `short_links` CHANGE `shortID` `shortID` MEDIUMINT(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `short_links`
   ADD PRIMARY KEY (`shortID`),
   ADD UNIQUE KEY `type_hash` (`type`,`hash`);
+
+ALTER TABLE `short_links`
+  MODIFY `shortID` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 UPDATE `settings` SET `data` = '5366' WHERE `settings`.`setting` = 'dbversion';
 COMMIT;
