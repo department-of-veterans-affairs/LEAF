015ff05283b3cd78bc3aa68c0328e518632f1c14
jeanmarcablevets <jeanmarc.daviau@ablevets.com>
2018-11-19 10:25:13 -0500

    ran code through php-cs-fixer

--- update-config-files.php
+++ update-config-files.php
@@ -13,17 +13,21 @@ define('WORD_WRAP', 120); // word wrap
  * [(php update-config-files.php write-portal) writes all Portal db-configs in a dir]
  * [(php update-config-files.php write-nexus) writes all Nexus configs in a dir]
  */
-for ($i = 0; $i <= $argc -1; ++$i) {
+for ($i = 0; $i <= $argc - 1; $i++)
+{
     switch ($argv[$i]) {
     case '-help':
       print "Note: Before running this file edit the array for add/remove of variables \n1. (write-portal) Writes Portal Config \n2. (write-nexus) Writes Nexus Config \n";
+
     break;
     case 'write-portal':
       writePortalConfig();
+
     break;
 
     case 'write-nexus':
       writeNexusConfig();
+
     break;
 
     default:
@@ -51,7 +55,7 @@ for ($i = 0; $i <= $argc -1; ++$i) {
           'adPath' => array('varName' => 'adPath', 'varValue' => '', 'varComment' => 'Active directory path', 'varType' => ''),
           'uploadDir' => array('varName' => 'uploadDir', 'varValue' => '', 'varComment' => 'Directory for user uploads. using backslashes (/), with trailing slash', 'varType' => ''),
           'leafSecure' => array('varName' => 'leafSecure', 'varValue' => '', 'varComment' => '', 'varType' => ''),
-          'orgchartPath' => array('varName' => 'orgchartPath', 'varValue' => "", 'varComment' => 'HTTP Path to orgchart with no trailing slash', 'varType' => ''),
+          'orgchartPath' => array('varName' => 'orgchartPath', 'varValue' => '', 'varComment' => 'HTTP Path to orgchart with no trailing slash', 'varType' => ''),
           'orgchartImportTags' => array('varName' => 'orgchartImportTags', 'varValue' => "array('')", 'varComment' => 'Import org chart groups if they match these tags', 'varType' => ''),
           'descriptionID' => array('varName' => 'descriptionID', 'varValue' => '', 'varComment' => 'indicator ID for description field', 'varType' => ''),
           'emailPrefix' => array('varName' => 'emailPrefix', 'varValue' => '', 'varComment' => 'Email prefix', 'varType' => ''),
@@ -64,12 +68,14 @@ for ($i = 0; $i <= $argc -1; ++$i) {
           'myNewVar' => array('varName' => 'myNewVar', 'varValue' => "'myNewValue'", 'varComment' => 'my new comment', 'varType' => 'static'),
         );
         $portalConfigs = rGlob('./*config.php');
-        foreach ($portalConfigs as $config) {
-            if (preg_match("/\/LEAF_Request_Portal\/\w.*/", $config)) {
+        foreach ($portalConfigs as $config)
+        {
+            if (preg_match('/\\/LEAF_Request_Portal\\/\\w.*/', $config))
+            {
                 //make a backup first
                 $date = new Datetime();
                 $timestamp = $date->format('U');
-                $backupConfig = dirname($config) . '/' . basename($config) .  '.' . $timestamp . '.php';
+                $backupConfig = dirname($config) . '/' . basename($config) . '.' . $timestamp . '.php';
                 copy($config, $backupConfig);
 
                 $file = file_get_contents($config);
@@ -105,12 +111,14 @@ for ($i = 0; $i <= $argc -1; ++$i) {
         );
 
         $nexusConfigs = rGlob('./config.php');
-        foreach ($nexusConfigs as $config) {
-            if (preg_match("/\/LEAF_Nexus\/\w.*/", $config)) {
+        foreach ($nexusConfigs as $config)
+        {
+            if (preg_match('/\\/LEAF_Nexus\\/\\w.*/', $config))
+            {
                 //make a backup first
                 $date = new Datetime();
                 $timestamp = $date->format('U');
-                $backupConfig = dirname($config) . '/' . basename($config) .  '.' . $timestamp . '.php';
+                $backupConfig = dirname($config) . '/' . basename($config) . '.' . $timestamp . '.php';
                 copy($config, $backupConfig);
 
                 $file = file_get_contents($config);
@@ -131,9 +139,11 @@ for ($i = 0; $i <= $argc -1; ++$i) {
     function rGlob($pattern, $flags = 0)
     {
         $files = glob($pattern, $flags);
-        foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
-            $files = array_merge($files, rglob($dir.'/'.basename($pattern), $flags));
+        foreach (glob(dirname($pattern) . '/*', GLOB_ONLYDIR | GLOB_NOSORT) as $dir)
+        {
+            $files = array_merge($files, rglob($dir . '/' . basename($pattern), $flags));
         }
+
         return $files;
     }
 
@@ -148,49 +158,74 @@ for ($i = 0; $i <= $argc -1; ++$i) {
         $current_var = array(VAR_NAME => false,
                             VAR_VALUE => false,
                             VAR_COMMENT => '',
-                            VAR_TYPE => false);
+                            VAR_TYPE => false, );
         $vars = array();
 
-        foreach ($tokens as $token) {
+        foreach ($tokens as $token)
+        {
             // Most of the tokens are arrays: [0] => type, [1] => value
-            if (is_array($token)) {
+            if (is_array($token))
+            {
                 $t_type = $token[0];
                 $t_val = $token[1];
-                if ($t_type == T_VARIABLE) {
-                    if ($current_var[VAR_NAME] === false) {
+                if ($t_type == T_VARIABLE)
+                {
+                    if ($current_var[VAR_NAME] === false)
+                    {
                         $current_var[VAR_NAME] = substr($t_val, 1);
-                    } elseif ($current_var[VAR_VALUE] !== false) {
+                    }
+                    elseif ($current_var[VAR_VALUE] !== false)
+                    {
                         $current_var[VAR_VALUE] .= $t_val;
                     }
-                } elseif ($t_val === 'static') {
-                    if ($current_var[VAR_TYPE] === false) {
+                }
+                elseif ($t_val === 'static')
+                {
+                    if ($current_var[VAR_TYPE] === false)
+                    {
                         $current_var[VAR_TYPE] = '';
-                    } else {
+                    }
+                    else
+                    {
                         $current_var[VAR_TYPE] .= $t_val;
                     }
-                } elseif ($current_var[VAR_VALUE] !== false) {
+                }
+                elseif ($current_var[VAR_VALUE] !== false)
+                {
                     $current_var[VAR_VALUE] .= $t_val;
                 }
-            } else {
-                if ($token === '=') {
-                    if ($current_var[VAR_VALUE] === false) {
+            }
+            else
+            {
+                if ($token === '=')
+                {
+                    if ($current_var[VAR_VALUE] === false)
+                    {
                         $current_var[VAR_VALUE] = '';
-                    } else {
+                    }
+                    else
+                    {
                         $current_var[VAR_VALUE] .= $token;
                     }
-                } elseif ($token === ';') {
-                    if ($current_var[VAR_NAME] && $current_var[VAR_VALUE] !== false) {
+                }
+                elseif ($token === ';')
+                {
+                    if ($current_var[VAR_NAME] && $current_var[VAR_VALUE] !== false)
+                    {
                         $current_var[VAR_VALUE] = ltrim($current_var[VAR_VALUE]);
                         $vars[$current_var[VAR_NAME]] = $current_var;
                     }
                     $current_var = array(VAR_NAME => false,
                                         VAR_VALUE => false,
-                                        VAR_COMMENT => '');
-                } elseif ($current_var[VAR_VALUE] !== false) {
+                                        VAR_COMMENT => '', );
+                }
+                elseif ($current_var[VAR_VALUE] !== false)
+                {
                     $current_var[VAR_VALUE] .= $token;
                 }
             }
         }
+
         return $vars;
     }
 
@@ -201,18 +236,22 @@ for ($i = 0; $i <= $argc -1; ++$i) {
      */
     function formatComments($comments)
     {
-        if (empty($comments)) {
+        if (empty($comments))
+        {
             return '';
         }
         $lines = explode("\n", $comments);
         $output = '';
-        foreach ($lines as $line) {
+        foreach ($lines as $line)
+        {
             $line = trim($line);
-            if (empty($line)) {
+            if (empty($line))
+            {
                 continue;
             }
-            $output .= '// '.$line.NEW_LINE;
+            $output .= '// ' . $line . NEW_LINE;
         }
+
         return $output;
     }
 
@@ -223,35 +262,39 @@ for ($i = 0; $i <= $argc -1; ++$i) {
      */
     function SaveNexus($fname = false, $vars)
     {
-        $src = "<?php " . NEW_LINE .
-       "/*"  . NEW_LINE .
-       "* As a work of the United States government, this project is in the public domain within the United States." . NEW_LINE .
-       "*/" . NEW_LINE .
-       "/*"  . NEW_LINE .
-        TAB . "General configuration " . NEW_LINE .
-        TAB . "Date: August 9, 2011" . NEW_LINE .
-        TAB . "Central place to put org. chart config" . NEW_LINE .
-        TAB . "This should be kept outside of web accessible directories" . NEW_LINE .
-        "*/" . NEW_LINE . NEW_LINE .
+        $src = '<?php ' . NEW_LINE .
+       '/*' . NEW_LINE .
+       '* As a work of the United States government, this project is in the public domain within the United States.' . NEW_LINE .
+       '*/' . NEW_LINE .
+       '/*' . NEW_LINE .
+        TAB . 'General configuration ' . NEW_LINE .
+        TAB . 'Date: August 9, 2011' . NEW_LINE .
+        TAB . 'Central place to put org. chart config' . NEW_LINE .
+        TAB . 'This should be kept outside of web accessible directories' . NEW_LINE .
+        '*/' . NEW_LINE . NEW_LINE .
         "// require '../../../config.php';" . NEW_LINE . NEW_LINE .
-        "namespace Orgchart;" . NEW_LINE .
+        'namespace Orgchart;' . NEW_LINE .
         "ini_set('display_errors', 0); // Set to 1 to display errors" . NEW_LINE . NEW_LINE .
 
-        "class Config" . NEW_LINE .
-        "{";
-        foreach ($vars as $var) {
-            $src .= NEW_LINE . TAB . 'public ' . ($var[VAR_TYPE] ? $var[VAR_TYPE] . ' ' : $var['varType'] . ' ') . '$'.($var[VAR_NAME] ? $var[VAR_NAME] : $var['varName']).' = '.($var[VAR_VALUE] ? $var[VAR_VALUE] : $var['varValue']).';' .' '.
+        'class Config' . NEW_LINE .
+        '{';
+        foreach ($vars as $var)
+        {
+            $src .= NEW_LINE . TAB . 'public ' . ($var[VAR_TYPE] ? $var[VAR_TYPE] . ' ' : $var['varType'] . ' ') . '$' . ($var[VAR_NAME] ? $var[VAR_NAME] : $var['varName']) . ' = ' . ($var[VAR_VALUE] ? $var[VAR_VALUE] : $var['varValue']) . ';' . ' ' .
             formatComments(wordwrap(($var['varComment'] ? $var['varComment'] : ''), WORD_WRAP));
         }
         $src .= NEW_LINE . NEW_LINE . ' }';
 
-        if ($fname !== false) {
+        if ($fname !== false)
+        {
             $fp = @fopen($fname, 'w');
-            if ($fp) {
+            if ($fp)
+            {
                 @fwrite($fp, $src);
                 @fclose($fp);
             }
         }
+
         return $src;
     }
 
@@ -264,53 +307,62 @@ for ($i = 0; $i <= $argc -1; ++$i) {
     {
         $dbConfig = array();
         $config = array();
-        foreach ($vars as $var) {
-            if ($var[VAR_NAME] == 'dbHost' || $var[VAR_NAME] == 'dbName' || $var[VAR_NAME] == 'dbUser' || $var[VAR_NAME] == 'dbPass') {
+        foreach ($vars as $var)
+        {
+            if ($var[VAR_NAME] == 'dbHost' || $var[VAR_NAME] == 'dbName' || $var[VAR_NAME] == 'dbUser' || $var[VAR_NAME] == 'dbPass')
+            {
                 $dbConfig[] = array(
             'comment' => formatComments(wordwrap(($var['varComment'] ? $var['varComment'] : ''), WORD_WRAP)),
-            'var' => NEW_LINE . TAB . 'public ' . ($var[VAR_TYPE] ? $var[VAR_TYPE] . ' ' : $var['varType'] . ' ') . '$'.($var[VAR_NAME] ? $var[VAR_NAME] : $var['varName']).' = '.($var[VAR_VALUE] ? $var[VAR_VALUE] : $var['varValue']).';' . ' '
+            'var' => NEW_LINE . TAB . 'public ' . ($var[VAR_TYPE] ? $var[VAR_TYPE] . ' ' : $var['varType'] . ' ') . '$' . ($var[VAR_NAME] ? $var[VAR_NAME] : $var['varName']) . ' = ' . ($var[VAR_VALUE] ? $var[VAR_VALUE] : $var['varValue']) . ';' . ' ',
               );
-            } else {
+            }
+            else
+            {
                 $config[] = array(
             'comment' => formatComments(wordwrap(($var['varComment'] ? $var['varComment'] : ''), WORD_WRAP)),
-            'var' => NEW_LINE . TAB . 'public ' . ($var[VAR_TYPE] ? $var[VAR_TYPE] . ' ' : $var['varType'] . ' ') . '$'.($var[VAR_NAME] ? $var[VAR_NAME] : $var['varName']).' = '.($var[VAR_VALUE] ? $var[VAR_VALUE] : $var['varValue']).';' . ' '
+            'var' => NEW_LINE . TAB . 'public ' . ($var[VAR_TYPE] ? $var[VAR_TYPE] . ' ' : $var['varType'] . ' ') . '$' . ($var[VAR_NAME] ? $var[VAR_NAME] : $var['varName']) . ' = ' . ($var[VAR_VALUE] ? $var[VAR_VALUE] : $var['varValue']) . ';' . ' ',
               );
             }
         }
-        $src = "<?php " . NEW_LINE .
-          "/*" . NEW_LINE .
-           "* As a work of the United States government, this project is in the public domain within the United States." . NEW_LINE .
-           "*/" . NEW_LINE .
-           "/*" . NEW_LINE .
-          TAB . "General configuration " . NEW_LINE .
-          TAB . "Date: August 9, 2011" . NEW_LINE .
-          TAB . "Central place to put database login information" . NEW_LINE .
-          TAB . "This should be kept outside of web accessible directories" . NEW_LINE .
-          "*/" . NEW_LINE . NEW_LINE .
+        $src = '<?php ' . NEW_LINE .
+          '/*' . NEW_LINE .
+           '* As a work of the United States government, this project is in the public domain within the United States.' . NEW_LINE .
+           '*/' . NEW_LINE .
+           '/*' . NEW_LINE .
+          TAB . 'General configuration ' . NEW_LINE .
+          TAB . 'Date: August 9, 2011' . NEW_LINE .
+          TAB . 'Central place to put database login information' . NEW_LINE .
+          TAB . 'This should be kept outside of web accessible directories' . NEW_LINE .
+          '*/' . NEW_LINE . NEW_LINE .
           "// require '../../../config.php';" . NEW_LINE . NEW_LINE .
           "ini_set('display_errors', 0); // Set to 1 to display errors" . NEW_LINE . NEW_LINE .
-          "class DB_Config" . NEW_LINE .
-          "{";
-        foreach ($dbConfig as $var) {
+          'class DB_Config' . NEW_LINE .
+          '{';
+        foreach ($dbConfig as $var)
+        {
             $src .= TAB . $var['var'] . $var['comment'];
         }
         $src .= NEW_LINE . '}' . NEW_LINE;
 
-        $src .= NEW_LINE . "class Config" . NEW_LINE .
-      "{";
-        foreach ($config as $var) {
+        $src .= NEW_LINE . 'class Config' . NEW_LINE .
+      '{';
+        foreach ($config as $var)
+        {
             $src .= $var['var'] . $var['comment'];
         }
 
         $src .= NEW_LINE . ' }';
 
-        if ($fname !== false) {
+        if ($fname !== false)
+        {
             $fp = @fopen($fname, 'w');
-            if ($fp) {
+            if ($fp)
+            {
                 @fwrite($fp, $src);
                 @fclose($fp);
             }
         }
+
         return $src;
     }
 
