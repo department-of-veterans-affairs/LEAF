ee625e52cec58dfe280d54ebebcaec9a67519248
jeanmarcablevets <jeanmarc.daviau@ablevets.com>
2018-12-06 16:48:15 -0500

    Corrected time until dialog and redirect

--- libs/js/jquery/sessionTimeout/sessionTimeout.js
+++ libs/js/jquery/sessionTimeout/sessionTimeout.js
@@ -6,8 +6,8 @@
             keepAliveAjaxRequestType: 'POST',
             redirUrl: '/timed-out',
             logoutUrl: '/log-out',
-            warnAfter: 900000, // 13 minutes
-            redirAfter: 1200000, // 2 minutes of inactivity from warnAfter, 15 minutes total
+            warnAfter: 780000, // 13 minutes
+            redirAfter: 900000, // 2 minutes of inactivity from warnAfter, 15 minutes total
             appendTime: true // appends time stamp to keep alive url to prevent caching
         };
 
