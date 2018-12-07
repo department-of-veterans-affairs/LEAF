1d901d607cf2e84abdb815e010eb0997e84067f4
nathan <nathanws@users.noreply.github.com>
2018-12-07 14:05:43 -0600

    Updating database migrations

--- LEAF_Request_Portal/utils/db_upgrade/Update_RMC_DB_5366-5367.sql
+++ LEAF_Request_Portal/utils/db_upgrade/Update_RMC_DB_5366-5367.sql
@@ -2,5 +2,5 @@ START TRANSACTION;
 
 ALTER TABLE `indicators` ADD `is_sensitive` TINYINT NOT NULL DEFAULT '0';
 
-UPDATE `settings` SET `data` = '5294' WHERE `settings`.`setting` = 'dbversion';
+UPDATE `settings` SET `data` = '5367' WHERE `settings`.`setting` = 'dbversion';
 COMMIT;
