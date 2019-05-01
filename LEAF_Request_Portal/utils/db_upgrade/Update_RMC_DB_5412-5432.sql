356a6b40bc098e38ffc1776a1e7d7b3388cc467c
mgaoVA <Michael.Gao@va.gov>
2019-05-01 16:12:12 -0400

    create DB column for pubkey

--- LEAF_Request_Portal/utils/db_upgrade/Update_RMC_DB_5412-5432.sql
+++ LEAF_Request_Portal/utils/db_upgrade/Update_RMC_DB_5412-5432.sql
@@ -1,6 +1,6 @@
 START TRANSACTION;
 
-
+ALTER TABLE `signatures` ADD `signerPublicKey` TEXT NOT NULL AFTER `message`;
 
 UPDATE `settings` SET `data` = '5432' WHERE `settings`.`setting` = 'dbversion';
 
