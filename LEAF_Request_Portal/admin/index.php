1c2d702f6317bd720c30a8a19dbe32c77833c7d3
jeanmarcablevets <jeanmarc.daviau@ablevets.com>
2019-05-22 02:46:43 -0400

    checks that variable is set before accessing it

--- LEAF_Request_Portal/admin/index.php
+++ LEAF_Request_Portal/admin/index.php
@@ -440,7 +440,8 @@ switch ($action) {
 }
 
 $main->assign('login', $t_login->fetch('login.tpl'));
-$main->assign('onPrem', Config::$onPrem);
+$onPrem = (isset(Config::$onPrem) && Config::$onPrem);
+$main->assign('onPrem', $onPrem);
 $t_menu->assign('action', $action);
 $t_menu->assign('orgchartPath', Config::$orgchartPath);
 $o_menu = $t_menu->fetch('menu.tpl');
