cc54b086bef04809745765e46282ffa6a67d1326
g-theriot <51964553+g-theriot@users.noreply.github.com>
2019-11-05 09:43:51 -0600

    Adding displayValue to logs
    Forgot to save this file on last commit

--- LEAF_Nexus/db_upgrade/Update_RMC_DB_2019082300-2019110100.sql
+++ LEAF_Nexus/db_upgrade/Update_RMC_DB_2019082300-2019110100.sql
@@ -14,10 +14,12 @@ CREATE TABLE `data_log_items` (
   `tableName` varchar(75) NOT NULL,
   `column` varchar(75) NOT NULL,
   `value` varchar(75) NOT NULL,
+  `displayValue` varchar(255) DEFAULT NULL
   PRIMARY KEY (`data_action_log_fk`,`tableName`,`column`)
 );
 
 
-UPDATE `settings` SET `data` = '5220' WHERE `settings`.`setting` = 'dbversion';
+
+UPDATE `settings` SET `data` = '2019110100' WHERE `settings`.`setting` = 'dbversion';
 
 COMMIT;
