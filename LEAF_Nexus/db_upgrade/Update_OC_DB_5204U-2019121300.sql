192bebc926cb0bbea6dfba5407292fd61fc8eb5c
jeremyrl <41297510+jeremyrl@users.noreply.github.com>
2019-12-13 14:37:00 -0600

    added proper migration

--- LEAF_Nexus/db_upgrade/Update_OC_DB_5204U-2019121300.sql
+++ LEAF_Nexus/db_upgrade/Update_OC_DB_5204U-2019121300.sql
@@ -1,4 +1,8 @@
--- This needs tobe run on the national instance of the nexus only
--- ALTER TABLE employee ADD COLUMN new_empUUID VARCHAR(36) NULL;
+START TRANSACTION;
+ALTER TABLE employee ADD COLUMN new_empUUID VARCHAR(36) NULL;
 
--- update employee set new_empUUID = uuid();
+update employee set new_empUUID = uuid();
+
+UPDATE `settings` SET `data` = '2019121300' WHERE `settings`.`setting` = 'dbversion';
+
+COMMIT;
