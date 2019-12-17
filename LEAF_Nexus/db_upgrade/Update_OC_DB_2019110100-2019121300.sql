f93b43f53d80ed67a3bb622c06fbb633815932ea
jeremyrl <41297510+jeremyrl@users.noreply.github.com>
2019-12-17 12:10:11 -0600

    updated migration

--- LEAF_Nexus/db_upgrade/Update_OC_DB_2019110100-2019121300.sql
+++ LEAF_Nexus/db_upgrade/Update_OC_DB_2019110100-2019121300.sql
@@ -1,7 +1,8 @@
 START TRANSACTION;
 ALTER TABLE employee ADD COLUMN new_empUUID VARCHAR(36) NULL;
 
-update employee set new_empUUID = uuid();
+--The following will have to be run on the national nexus manually, to avoid hitting all nexuses, should only take a minute
+--update employee set new_empUUID = uuid();
 
 UPDATE `settings` SET `data` = '2019121300' WHERE `settings`.`setting` = 'dbversion';
 
