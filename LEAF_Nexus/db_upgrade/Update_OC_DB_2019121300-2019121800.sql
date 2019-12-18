c97b7c213f423754798f854f87c7ee2b5770a332
mgaoVA <Michael.Gao@va.gov>
2019-12-18 13:14:43 -0500

    fix order

--- LEAF_Nexus/db_upgrade/Update_OC_DB_2019121300-2019121800.sql
+++ LEAF_Nexus/db_upgrade/Update_OC_DB_2019121300-2019121800.sql
@@ -1,11 +1,9 @@
 START TRANSACTION;
 
-ALTER TABLE employee ADD COLUMN new_empUUID VARCHAR(36) NULL;
-
-/*
-The following will have to be run on the national nexus manually, to avoid hitting all nexuses, should only take a minute
-update employee set new_empUUID = uuid();
-*/
+INSERT INTO `indicators` (`indicatorID`, `name`, `format`, `description`, `default`, `parentID`, `categoryID`, `html`, `jsSort`, `required`, `sort`, `timeAdded`, `encrypted`, `disabled`) VALUES ('27', 'LEAF Developer Console Access', 'checkbox\r\nYes', NULL, NULL, NULL, 'employee', NULL, NULL, '0', '98', current_timestamp(), '0', '0'), ('28', 'LEAF Developer Console Request Reference', 'text', NULL, NULL, NULL, 'employee', NULL, NULL, '0', '99', current_timestamp(), '0', '0'); 
+INSERT INTO `indicator_privileges` (`indicatorID`, `categoryID`, `UID`, `read`, `write`, `grant`) VALUES
+(27, 'group', 2, 1, 0, 0),
+(28, 'group', 2, 1, 0, 0);
 
 UPDATE `settings` SET `data` = '2019121800' WHERE `settings`.`setting` = 'dbversion';
 
