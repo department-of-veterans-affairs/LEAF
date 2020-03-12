ff2f24bf8b3cd1c70b53dd21ecd843460a0bf2c8
mgaoVA <Michael.Gao@va.gov>
2020-03-12 14:11:46 -0400

    validate and scrub QR payloads

--- libs/qrcode/index.php
+++ libs/qrcode/index.php
@@ -1,12 +1,43 @@
 <?php 
+include '../php-commons/XSSHelpers.php';
 include 'qrlib.php';
 $cacheDir = 'cache/';
 
 $encode = 'Invalid Input.';
 if(isset($_GET['encode'])) {
+    $input = XSSHelpers::xssafe($_GET['encode']); // first pass scrub and character encoding enforcement
+
     $len = strlen($_GET['encode']);
-    if($len > 0 && $len < 4000) {
-        $encode = $_GET['encode'];
+    if($len > 0 && $len < 4000) { // check QR container limits
+        $encode = $input;
+    }
+
+    // TODO: Replace this with server config variable
+    $HTTP_HOST = '';
+    $matches = array();
+    switch($_SERVER['HTTP_HOST']) {
+        case 'leaf.va.gov':
+            $HTTP_HOST = 'leaf.va.gov';
+            break;
+        case 'leaf-preprod.va.gov':
+            $HTTP_HOST = 'leaf-preprod.va.gov';
+            break;
+        case 'leaf-dev.va.gov':
+            $HTTP_HOST = 'leaf-dev.va.gov';
+            break;
+        case (preg_match('/localhost:(\d+)/', $_SERVER['HTTP_HOST'], $matches) ? true : false):
+            $HTTP_HOST = 'localhost';
+            if(isset($matches[1]) && is_numeric($matches[1])) {
+                $HTTP_HOST .= ':' . $matches[1];
+            }
+            break;
+        default:
+            break;
+    }
+
+    $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 'https' : 'http';
+    if(strpos($input, "{$protocol}://{$HTTP_HOST}/") !== 0) {
+        $encode = "Invalid Input.";
     }
 }
 
@@ -43,5 +74,3 @@ else {
     header('Content-Type: image/png');
     echo file_get_contents($cacheFile);
 }
-
-?>
