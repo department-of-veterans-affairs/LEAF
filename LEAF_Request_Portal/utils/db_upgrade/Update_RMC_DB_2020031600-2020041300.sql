54679f7767b3f8e36735446809a1819998eb5b57
Abiola Adewuyi <abiola.adewuyi@AdewuAbi3811.fios-router.home>
2020-04-14 15:17:38 -0400

    LEAF-1385: Modified script to update email template table

--- LEAF_Request_Portal/utils/db_upgrade/Update_RMC_DB_2020031600-2020041300.sql
+++ LEAF_Request_Portal/utils/db_upgrade/Update_RMC_DB_2020031600-2020041300.sql
@@ -1,18 +1,8 @@
 START TRANSACTION;
 
-CREATE TABLE `email_templates` (
-  `emailTemplateID` mediumint(8) NOT NULL AUTO_INCREMENT,
-  `label` varchar(255) NOT NULL,
-  `subject` text NOT NULL,
-  `body` text NOT NULL,
-  PRIMARY KEY (`emailTemplateID`),
-  UNIQUE KEY `label` (`label`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-
-INSERT INTO `email_templates` (`emailTemplateID`, `label`, `subject`, `body`) VALUES
-(-3,	'Notify Requestor of Completion',	'LEAF_notify_complete_subject.tpl',	'LEAF_notify_complete_body.tpl'),
-(-2,	'Notify Next Approver',	'LEAF_notify_next_subject.tpl',	'LEAF_notify_next_body.tpl'),
-(-1,	'Send Back',	'LEAF_send_back_subject.tpl',	'LEAF_send_back_body.tpl');
+UPDATE `email_templates` SET `subject` = 'LEAF_notify_complete_subject.tpl', `body` = 'LEAF_notify_complete_body.tpl' WHERE `emailTemplateID` = -3;
+UPDATE `email_templates` SET `subject` = 'LEAF_notify_next_subject.tpl', `body` = 'LEAF_notify_next_body.tpl' WHERE `emailTemplateID` = -2;
+UPDATE `email_templates` SET `subject` = 'LEAF_send_back_subject.tpl',	`body` = 'LEAF_send_back_body.tpl' WHERE `emailTemplateID` = -1;
 
 UPDATE `settings` SET `data` = '2020041300' WHERE `settings`.`setting` = 'dbversion';
 
