90fa9470ae38b7edba8a2284727c0567c3bbceee
Liping Wang <liping.wang@ablevets.com>
2020-04-23 08:32:49 -0400

    using PHP_INI_DIR variable

--- test/LEAF_Nexus_Tests/run_tests.sh
+++ test/LEAF_Nexus_Tests/run_tests.sh
@@ -1,3 +1,3 @@
-phpunit --colors --bootstrap ../bootstrap.php tests
+#phpunit --colors --bootstrap ../bootstrap.php tests
 
 phpunit --colors --bootstrap ../bootstrap.php --coverage-html /var/www/html/Nexus_Reports tests
