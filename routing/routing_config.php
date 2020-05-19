32379ae74d6104a27e2916a5cb52b509a11478bc
jeremyrl <41297510+jeremyrl@users.noreply.github.com>
2020-05-19 08:54:39 -0500

    switched username to tester

--- routing/routing_config.php
+++ routing/routing_config.php
@@ -1,7 +1,7 @@
 <?php
     ini_set('display_errors', 0); // Set to 1 to display errors
-    $_SERVER['REMOTE_USER'] = '\\VACOLayJ';
-    $_SESSION['userID'] = '\\VACOLayJ';
+    $_SERVER['REMOTE_USER'] = '\\tester';
+    $_SESSION['userID'] = '\\tester';
     class Routing_Config
     {
         public static $dbHost = 'localhost';
