99ea83c0ec31220e62ee6ff4fd4df67501b82b5c
jeremyrl <41297510+jeremyrl@users.noreply.github.com>
2020-06-08 15:09:56 -0500

    bug fixes

--- LEAF_Request_Portal/Email.php
+++ LEAF_Request_Portal/Email.php
@@ -80,7 +80,7 @@ class Email
     {
         $i = str_replace("\r\n", '<br />', $i);
         $smarty = new Smarty;
-        $smarty->setTemplateDir(__DIR__."/templates/")->setCompileDir(__DIR__."/templates_c/");
+        $smarty->setTemplateDir(__DIR__."/templates/email/")->setCompileDir(__DIR__."/templates_c/");
         $smarty->left_delimiter = '{{';
         $smarty->right_delimiter = '}}';
         $smarty->assign('emailBody', $i);
@@ -340,7 +340,7 @@ class Email
     function setSubjectWithTemplate($subjectTemplate)
     {
         $smartySubject = new Smarty;
-        $smartySubject->setTemplateDir(__DIR__."/templates/")->setCompileDir(__DIR__."/templates_c/");
+        $smartySubject->setTemplateDir(__DIR__."/templates/email/")->setCompileDir(__DIR__."/templates_c/");
         $smartySubject->left_delimiter = '{{';
         $smartySubject->right_delimiter = '}}';
         $smartySubject->assign($this->smartyVariables);
@@ -356,7 +356,7 @@ class Email
     function setBodyWithTemplate($bodyTemplate)
     {
         $smartyBody = new Smarty;
-        $smartyBody->setTemplateDir(__DIR__."/templates/")->setCompileDir(__DIR__."/templates_c/");
+        $smartyBody->setTemplateDir(__DIR__."/templates/email/")->setCompileDir(__DIR__."/templates_c/");
         $smartyBody->left_delimiter = '{{';
         $smartyBody->right_delimiter = '}}';
         $smartyBody->assign($this->smartyVariables);
