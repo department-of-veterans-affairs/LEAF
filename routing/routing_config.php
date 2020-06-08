c11a2fe3e7b66bc26e4429dbef8d4bb9a5d46c34
jeremyrl <41297510+jeremyrl@users.noreply.github.com>
2020-06-08 10:43:25 -0500

    bug fixes

--- routing/routing_config.php
+++ routing/routing_config.php
@@ -1,7 +1,5 @@
 <?php
     ini_set('display_errors', 0); // Set to 1 to display errors
-    $_SERVER['REMOTE_USER'] = '\\tester';
-    $_SESSION['userID'] = '\\tester';
     class Routing_Config
     {
         public static $dbHost = 'mysql';
