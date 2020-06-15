3d3483139fd29b4a290263bb674dcd453c0eadf1
jeremyrl <41297510+jeremyrl@users.noreply.github.com>
2020-06-15 16:54:21 -0500

    bug fix

--- LEAF_Request_Portal/ajaxScript.php
+++ LEAF_Request_Portal/ajaxScript.php
@@ -22,7 +22,7 @@ $action = isset($_GET['a']) ? XSSHelpers::xscrub($_GET['a']) : '';
 $script = isset($_GET['s']) ? XSSHelpers::scrubFilename(XSSHelpers::xscrub($_GET['s'])) : '';
 
 $main = new Smarty;
-$main->setTemplateDir(__DIR__."/templates/")->setCompileDir(__DIR__."/templates_c/");
+$main->setTemplateDir(__DIR__."/")->setCompileDir(__DIR__."/templates_c/");
 $main->left_delimiter = '{{';
 $main->right_delimiter = '}}';
 
