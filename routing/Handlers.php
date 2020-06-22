61c578c930f6b802a144a2e5f35216e0b56647de
jeremyrl <41297510+jeremyrl@users.noreply.github.com>
2020-06-22 10:28:31 -0500

    codacy fixes

--- routing/Handlers.php
+++ routing/Handlers.php
@@ -10,87 +10,87 @@ namespace Handlers{
         }
         function default(){
             global $config, $db_config;
-            require __DIR__.'/../LEAF_Request_Portal/index.php';
+            require __DIR__ . '/../LEAF_Request_Portal/index.php';
         }
 
         function other($otherFile){
             global $config, $db_config;
-            require __DIR__.'/../LEAF_Request_Portal/'.$otherFile;
+            require __DIR__ . '/../LEAF_Request_Portal/' . $otherFile;
         }
 
         function favicon($favicon){
             header('Content-Type: image/png');
-            readfile(__DIR__.'/../LEAF_Request_Portal/'.$favicon);
+            readfile(__DIR__ . '/../LEAF_Request_Portal/' . $favicon);
         }
 
         function js($jsFile){
             header('Content-Type: text/javascript');
-            echo file_get_contents(__DIR__.'/../LEAF_Request_Portal/js/'.$jsFile);
+            echo file_get_contents(__DIR__ . '/../LEAF_Request_Portal/js/' . $jsFile);
         }
 
         function css($cssFile){
             header('Content-Type: text/css');
-            echo file_get_contents(__DIR__.'/../LEAF_Request_Portal/css/'.$cssFile);
+            echo file_get_contents(__DIR__ . '/../LEAF_Request_Portal/css/' . $cssFile);
         }
 
         function image($image){
             header('Content-Type: image/png');
-            readfile(__DIR__.'/../LEAF_Request_Portal/images/'.$image);
+            readfile(__DIR__ . '/../LEAF_Request_Portal/images/' . $image);
         }
 
         function api(){
             global $config, $db_config;
-            require __DIR__.'/../LEAF_Request_Portal/api/index.php';
+            require __DIR__ . '/../LEAF_Request_Portal/api/index.php';
         }
 
         function login(){
             global $config, $db_config;
-            echo file_get_contents(__DIR__.'/../LEAF_Request_Portal/login/index.php');
+            echo file_get_contents(__DIR__ . '/../LEAF_Request_Portal/login/index.php');
         }
 
         function scripts($scriptFile){
             global $config, $db_config;
-            require __DIR__.'/../LEAF_Request_Portal/scripts/'.$scriptFile;
+            require __DIR__ . '/../LEAF_Request_Portal/scripts/' . $scriptFile;
         }
 
         function auth_domain(){
             global $config, $db_config;
-            require __DIR__.'/../LEAF_Request_Portal/auth_domain/index.php';
+            require __DIR__ . '/../LEAF_Request_Portal/auth_domain/index.php';
         }
 
         function auth_domain_api(){
             global $config, $db_config;
-            require __DIR__.'/../LEAF_Request_Portal/auth_domain/api/index.php';
+            require __DIR__ . '/../LEAF_Request_Portal/auth_domain/api/index.php';
         }
 
         function auth_cookie(){
             global $config, $db_config;
-            require __DIR__.'/../LEAF_Request_Portal/auth_cookie/index.php';
+            require __DIR__ . '/../LEAF_Request_Portal/auth_cookie/index.php';
         }
 
         function auth_token(){
             global $config, $db_config;
-            require __DIR__.'/../LEAF_Request_Portal/auth_token/index.php';
+            require __DIR__ . '/../LEAF_Request_Portal/auth_token/index.php';
         }
 
         function admin_index(){
             global $config, $db_config;
-            require __DIR__.'/../LEAF_Request_Portal/admin/index.php';
+            require __DIR__ . '/../LEAF_Request_Portal/admin/index.php';
         }
 
         function admin_css($cssFile){
             header('Content-Type: text/css');
-            require __DIR__.'/../LEAF_Request_Portal/admin/css/'.$cssFile;
+            require __DIR__ . '/../LEAF_Request_Portal/admin/css/' . $cssFile;
         }
 
         function admin_js($jsFile){
             header('Content-Type: text/javascript');
-            require __DIR__.'/../LEAF_Request_Portal/admin/js/'.$jsFile;
+            require __DIR__ . '/../LEAF_Request_Portal/admin/js/' . $jsFile;
         }
         
         function admin_other($adminFile){
             global $config, $db_config;
-            require __DIR__.'/../LEAF_Request_Portal/admin/'.$adminFile;
+            require __DIR__ . '/../LEAF_Request_Portal/admin/' . $adminFile;
         }
     }
 
@@ -103,67 +103,67 @@ namespace Handlers{
 
         function other($otherFile){
             global $config;
-            require __DIR__.'/../LEAF_Nexus/'.$otherFile;
+            require __DIR__ . '/../LEAF_Nexus/' . $otherFile;
         }
 
         function api(){
             global $config;
-            require __DIR__.'/../LEAF_Nexus/api/index.php';
+            require __DIR__ . '/../LEAF_Nexus/api/index.php';
         }
 
         function auth_domain(){
             global $config;
-            require __DIR__.'/../LEAF_Nexus/auth_domain/index.php';
+            require __DIR__ . '/../LEAF_Nexus/auth_domain/index.php';
         }
 
         function auth_cookie(){
             global $config;
-            require __DIR__.'/../LEAF_Nexus/auth_cookie/index.php';
+            require __DIR__ . '/../LEAF_Nexus/auth_cookie/index.php';
         }
 
         function auth_token(){
             global $config;
-            require __DIR__.'/../LEAF_Nexus/auth_token/index.php';
+            require __DIR__ . '/../LEAF_Nexus/auth_token/index.php';
         }
 
         function js($jsFile){
             header('Content-Type: text/javascript');
-            echo file_get_contents(__DIR__.'/../LEAF_Nexus/js/'.$jsFile);
+            echo file_get_contents(__DIR__ . '/../LEAF_Nexus/js/' . $jsFile);
         }
 
         function css($cssFile){
             header('Content-Type: text/css');
-            echo file_get_contents(__DIR__.'/../LEAF_Nexus/css/'.$cssFile);
+            echo file_get_contents(__DIR__ . '/../LEAF_Nexus/css/' . $cssFile);
         }
 
         function image($image){
             header('Content-Type: image/png');
-            readfile(__DIR__.'/../LEAF_Nexus/images/'.$image);
+            readfile(__DIR__ . '/../LEAF_Nexus/images/' . $image);
         }
 
         function login(){
             global $config;
-            echo file_get_contents(__DIR__.'/../LEAF_Nexus/login/index.php');
+            echo file_get_contents(__DIR__ . '/../LEAF_Nexus/login/index.php');
         }
 
         function admin_index(){
             global $config;
-            require __DIR__.'/../LEAF_Nexus/admin/index.php';
+            require __DIR__ . '/../LEAF_Nexus/admin/index.php';
         }
 
         function admin_css($cssFile){
             header('Content-Type: text/css');
-            require __DIR__.'/../LEAF_Nexus/admin/css/'.$cssFile;
+            require __DIR__ . '/../LEAF_Nexus/admin/css/' . $cssFile;
         }
         
         function admin_other($adminFile){
             global $config;
-            require __DIR__.'/../LEAF_Nexus/admin/'.$adminFile;
+            require __DIR__ . '/../LEAF_Nexus/admin/' . $adminFile;
         }
 
         function default(){
             global $config;
-            require __DIR__.'/../LEAF_Nexus/index.php';
+            require __DIR__ . '/../LEAF_Nexus/index.php';
         }
     }
 }
