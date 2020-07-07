a8a824ed1048b7bbe0504d199272b670949551a9
Patrick Liesveld <patrickcgs@users.noreply.github.com>
2020-07-07 19:09:39 -0400

    LEAF-1570: cleanup

--- fort_report/FortifyReportGen.sh
+++ fort_report/FortifyReportGen.sh
@@ -2,55 +2,45 @@
 
 export SCA_VM_OPTS=-Xmx800M
 
-#cd
-#cd workspace/$(cat workspace/workspaces.txt | tail -n 1)
+# Contents of PROJECT_ID, VERSION_ID, and BUILD_ID will be present the published artifact
 
-#BUILD_NUMBER="${BUILD_NUMBER:-SNAPSHOT}"
-#FILE_PREFIX="leaf-${BUILD_NUMBER}"
-#ARTIFACT_ID="${FILE_PREFIX}"
-#FPR="fortify/${FILE_PREFIX}.fpr"
-#PDF="fortify/${FILE_PREFIX}.pdf"
+# SPRINT_ID is used to name the artifacts published to S3
+PROJECT_ID="${SPRINT_ID}_${GIT_BRANCH}_${GIT_COMMIT}"
+VERSION_ID="${GIT_BRANCH}_${GIT_COMMIT}"
 
-# Using hardcoded values for now. Will update when integrate with pipeline
+
 FILE_PREFIX="leaf"
-ARTIFACT_ID="${FILE_PREFIX}"
+BUILD_ID="${FILE_PREFIX}"
 FPR="$PWD/fort_report/${FILE_PREFIX}.fpr"
 FPR_ORIG="$PWD/fort_report/${FILE_PREFIX}_orig.fpr"
 FPR_MERGED="$PWD/fort_report/${FILE_PREFIX}_merged.fpr"
 PDF="$PWD/fort_report/${FILE_PREFIX}.pdf"
 PROPERTIES_FILE="$PWD/fort_report/fortify.properties"
 
-SPRINT_ID="BLD2020-07-06XX"
-BUILD_ID="${SPRINT_ID}_${GIT_BRANCH}_${GIT_COMMIT}_${SID}"
-
 MEMORY="-Xmx1600M -Xms1000M -Xss48M"
-
 TEMPLATE="/workspace/fortify_templates/Security_Report.xml"
 REPORT_OPTIONS="-showRemoved -showSuppressed -showHidden -verbose"
+LAUNCHERSWITCHES="-build-label $BUILD_ID -build-version $VERSION_ID -build-project $PROJECT_ID"
 
 set -x
 # enable debug
-
-env
 
 set -eo pipefail
 # strict mode
 #   script aborts if any command returns non-zero exit-code
 #   http://redsymbol.net/articles/unofficial-bash-strict-mode/
 
-# fortifyupdate 
-
 echo --------------------------------------
 echo Cleaning previous scan artifacts...
-sourceanalyzer $MEMORY -b $ARTIFACT_ID -build-label $ARTIFACT_ID -clean -verbose 
+sourceanalyzer $MEMORY -b $BUILD_ID -build-label $BUILD_ID -clean -verbose 
 
 echo --------------------------------------
 echo Translating project...
-sourceanalyzer -php-source-root libs/ $MEMORY $LAUNCHERSWITCHES -b $ARTIFACT_ID -build-label $ARTIFACT_ID -build-version $BUILD_ID -build-project $BUILD_ID  @$PROPERTIES_FILE
+sourceanalyzer -php-source-root libs/ $MEMORY $LAUNCHERSWITCHES -b $BUILD_ID @$PROPERTIES_FILE
 
 echo --------------------------------------
 echo Starting scan
-sourceanalyzer $MEMORY $LAUNCHERSWITCHES -b $ARTIFACT_ID -build-label $ARTIFACT_ID -build-version $BUILD_ID -build-project $BUILD_ID -scan -f $FPR -verbose
+sourceanalyzer $MEMORY $LAUNCHERSWITCHES -b $BUILD_ID -scan -f $FPR -verbose
 
 if [ -f $FPR_ORIG ]; then
 echo --------------------------------------
