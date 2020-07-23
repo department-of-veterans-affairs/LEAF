c5ac5a8af854ea4d35b11d2e3053813991803988
Dastan Rahimi <dastan.rahimi@ablevets.com>
2020-07-23 14:40:09 -0400

    Migrated the code to work with stateless

--- routing/routing_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    ini_set('display_errors', 0); // Set to 1 to display errors
-    class Routing_Config
-    {
-        public static $dbHost = 'mysql';
-        public static $dbName = 'leaf_config';
-        public static $dbUser = 'tester';
-        public static $dbPass = 'tester';
-    }
