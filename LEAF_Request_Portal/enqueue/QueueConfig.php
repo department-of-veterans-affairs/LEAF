4d7d8c71a632f6b9a6983c37bc72ac444bd43723
Greg Henley <gregory.henley@ablevets.com>
2020-10-13 12:31:30 -0400

    added missing file from initial commit

--- LEAF_Request_Portal/enqueue/QueueConfig.php
+++ LEAF_Request_Portal/enqueue/QueueConfig.php
@@ -2,13 +2,13 @@
 
 class QueueConfig
 {
-    public $key = 'AKIAQUISU65OYHCJBATS';
+    public $key = 'ENTER_AWS_KEY';
 
-    public $secret = 'ClMoK1WtFEJJGLoCsiodqGuTdFOMYRTw0CpJg8Pr';
+    public $secret = 'ENTER_AWS_SECRET';
 
     public $region = 'us-east-1';
 
     public $version = '2012-11-05';
 
-    public $endPoint = 'https://sqs.us-east-1.amazonaws.com/043525666653/EmailQueue';
+    public $endPoint = 'ENTER_AWS_SQS_QUEUE_ENDPOINT';
 }
