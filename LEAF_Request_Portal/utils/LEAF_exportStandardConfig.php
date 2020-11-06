7cbd7105bd3419c0e171e81f3cde24430c7af2fa
Thomas Nguyen <thomas.nguyen@ablevets.com>
2020-11-06 14:41:41 -0500

    different temp folder for easy reference, fix for passing variable

--- LEAF_Request_Portal/utils/LEAF_exportStandardConfig.php
+++ LEAF_Request_Portal/utils/LEAF_exportStandardConfig.php
@@ -1,12 +1,11 @@
 <?php
 ini_set('display_errors', 0); // Set to 1 to display errors
-
-$tempFolder = __DIR__ . '/temp/'; // utils/temp/
-
 define("LF", "\n");
+
 include __DIR__ . '/../db_mysql.php';
 
 $debug = false;
+$tempFolder = __DIR__ . '/temp/'; // utils/temp
 
 $db = new DB($db_config->dbHost, $db_config->dbUser, $db_config->dbPass, $db_config->dbName);
 
@@ -19,10 +18,13 @@ if($res['siteType'] != 'national_primary') {
 if ($debug) {
     $db->enableDebug();
 }
-echo "Running Package Builder...<br />\n";
-array_map('unlink', glob($tempFolder . '*.sql'));
 
-function exportTable($db, $tempFolder, $table) {
+echo "Running Package Builder...<br />\n";
+$cleanPrimaryPrefix = str_replace("/", "_", $config->portalPath); // portal_prefix_
+
+array_map('unlink', glob($tempFolder . $cleanPrimaryPrefix . '*.sql'));
+
+function exportTable($db, $tempFolder, $cleanPrimaryPrefix, $table) {
     switch($table) {
         case 'actions':
         case 'categories':
@@ -43,21 +45,19 @@ function exportTable($db, $tempFolder, $table) {
 
     $res = $db->query("SELECT * FROM {$table}");
 
-    $cleanPortalPath = str_replace("/", "_", $config->portalPath);
-
-    file_put_contents("{$tempFolder}/${cleanPortalPath}_{$table}.sql", serialize($res));
+    file_put_contents("{$tempFolder}{$cleanPrimaryPrefix}_{$table}.sql", serialize($res));
 }
 
-exportTable($db, $tempFolder, 'actions');
-exportTable($db, $tempFolder, 'categories');
-exportTable($db, $tempFolder, 'category_staples');
-exportTable($db, $tempFolder, 'dependencies');
-exportTable($db, $tempFolder, 'indicators');
-exportTable($db, $tempFolder, 'route_events');
-exportTable($db, $tempFolder, 'workflows');
-exportTable($db, $tempFolder, 'workflow_steps');
-exportTable($db, $tempFolder, 'step_dependencies');
-exportTable($db, $tempFolder, 'workflow_routes');
-exportTable($db, $tempFolder, 'step_modules');
+exportTable($db, $tempFolder, $cleanPrimaryPrefix, 'actions');
+exportTable($db, $tempFolder, $cleanPrimaryPrefix, 'categories');
+exportTable($db, $tempFolder, $cleanPrimaryPrefix, 'category_staples');
+exportTable($db, $tempFolder, $cleanPrimaryPrefix, 'dependencies');
+exportTable($db, $tempFolder, $cleanPrimaryPrefix, 'indicators');
+exportTable($db, $tempFolder, $cleanPrimaryPrefix, 'route_events');
+exportTable($db, $tempFolder, $cleanPrimaryPrefix, 'workflows');
+exportTable($db, $tempFolder, $cleanPrimaryPrefix, 'workflow_steps');
+exportTable($db, $tempFolder, $cleanPrimaryPrefix, 'step_dependencies');
+exportTable($db, $tempFolder, $cleanPrimaryPrefix, 'workflow_routes');
+exportTable($db, $tempFolder, $cleanPrimaryPrefix, 'step_modules');
 
 echo "Package Built <br />\n";
