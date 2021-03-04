76598059d6718c259cdf1a0cb5b3d17181e3abce
Pete Nerantzinis <pete-nerantzinis@users.noreply.github.com>
2021-03-04 16:51:40 -0500

    LEAF-1976: Added new controller for Internal Forms

--- LEAF_Request_Portal/api/controllers/FormController.php
+++ LEAF_Request_Portal/api/controllers/FormController.php
@@ -107,6 +107,10 @@ class FormController extends RESTfulResponse
             return $form->getFullForm((int)$args[0], null);
         });
 
+        $this->index['GET']->register('form/[digit]/[text]/data/tree', function ($args) use ($form) {
+            return $form->getFullForm($args[0], $args[1]);
+        });
+
         $this->index['GET']->register('form/[digit]/dataforsigning', function ($args) use ($form) {
             return $form->getFullFormDataForSigning($args[0]);
         });
