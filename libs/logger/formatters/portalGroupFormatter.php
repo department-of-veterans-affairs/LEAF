5cc4dc38b1fae45e085c907dd1cb41a4403985d9
Pete Nerantzinis <pete-nerantzinis@users.noreply.github.com>
2021-03-26 16:53:18 -0400

    LEAF-1993: Updated API/Logger controllers and format

--- libs/logger/formatters/portalGroupFormatter.php
+++ libs/logger/formatters/portalGroupFormatter.php
@@ -6,12 +6,20 @@ require_once 'dataActions.php';
 class PortalGroupFormatter{
     
     const TEMPLATES = [
+        DataActions::ADD.'-'.LoggableTypes::PORTAL_GROUP => [
+            "message"=>"Group <strong>%s</strong> created",
+            "variables"=>"name"
+        ],
+        DataActions::DELETE.'-'.LoggableTypes::PORTAL_GROUP => [
+            "message"=>"Group <strong>%s</strong> deleted",
+            "variables"=>"groupID"
+        ],
         DataActions::ADD.'-'.LoggableTypes::EMPLOYEE => [
-            "message" => "User %s has been added to group %s",
+            "message" => "User <strong>%s</strong> has been added to group <strong>%s</strong>",
             "variables" => "userID,groupID"
         ],
         DataActions::DELETE.'-'.LoggableTypes::EMPLOYEE => [
-            "message" => "User %s has been removed from group %s",
+            "message" => "User <strong>%s</strong> has been removed from group <strong>%s</strong>",
             "variables" => "userID,groupID"
         ]
     ];
