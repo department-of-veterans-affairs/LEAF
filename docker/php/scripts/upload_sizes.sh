89bb37729203a7e05c1bb266fd49794a0d399eb5
Gregory Steinhoff <26551882+gsteinhoff@users.noreply.github.com>
2021-03-30 12:33:15 -0500

    LEAF-1989 - Changing script over to how it will be used on production

--- docker/php/scripts/upload_sizes.sh
+++ docker/php/scripts/upload_sizes.sh
@@ -4,6 +4,6 @@
 cd /var/www/ERM_UPLOADS
 
 #dev
-du --max-depth=3 * | sort -rn > /var/www/html/_portal-upload-sizes
+# du --max-depth=3 * | sort -rn > /var/www/html/_portal-upload-sizes
 #prod
-# du --max-depth=3 * | sort -rn > /var/www/html/launchpad/_portal-upload-sizes
+du --max-depth=3 * | sort -rn > /var/www/html/launchpad/_portal-upload-sizes
