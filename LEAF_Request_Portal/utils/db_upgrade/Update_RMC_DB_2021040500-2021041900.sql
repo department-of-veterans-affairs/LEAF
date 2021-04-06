10db026ec20eb2a1ec64a4925db8672f82217f44
Gregory Steinhoff <26551882+gsteinhoff@users.noreply.github.com>
2021-04-06 13:01:55 -0500

    LEAF-1687 - Add Display Name to Email Templates DB

--- LEAF_Request_Portal/utils/db_upgrade/Update_RMC_DB_2021040500-2021041900.sql
+++ LEAF_Request_Portal/utils/db_upgrade/Update_RMC_DB_2021040500-2021041900.sql
@@ -1,12 +1,13 @@
 START TRANSACTION;
 
 ALTER TABLE `email_templates`
-    ADD COLUMN `emailTo` text NULL AFTER `emailTemplateID`,
+    ADD COLUMN `name` text NOT NULL AFTER `emailTemplateID`,
+    ADD COLUMN `emailTo` text NULL AFTER `name`,
     ADD COLUMN `emailCc` text NULL AFTER `emailTo`;
 
-UPDATE `email_templates` SET `emailTo` = 'LEAF_notify_complete_email_to.tpl', `emailCc` = 'LEAF_notify_complete_email_cc.tpl' WHERE `emailTemplateID` = -3;
-UPDATE `email_templates` SET `emailTo` = 'LEAF_notify_next_email_to.tpl', `emailCc` = 'LEAF_notify_next_email_cc.tpl' WHERE `emailTemplateID` = -2;
-UPDATE `email_templates` SET `emailTo` = 'LEAF_send_back_email_to.tpl',	`emailCc` = 'LEAF_send_back_email_cc.tpl' WHERE `emailTemplateID` = -1;
+UPDATE `email_templates` SET `name` = 'Send Form Submitted', `emailTo` = 'LEAF_notify_complete_email_to.tpl', `emailCc` = 'LEAF_notify_complete_email_cc.tpl' WHERE `emailTemplateID` = -3;
+UPDATE `email_templates` SET `name` = 'Send to Approvers', `emailTo` = 'LEAF_notify_next_email_to.tpl', `emailCc` = 'LEAF_notify_next_email_cc.tpl' WHERE `emailTemplateID` = -2;
+UPDATE `email_templates` SET `name` = 'Send Back to Requestor', `emailTo` = 'LEAF_send_back_email_to.tpl',	`emailCc` = 'LEAF_send_back_email_cc.tpl' WHERE `emailTemplateID` = -1;
 
 UPDATE `settings` SET `data` = '2021041900' WHERE `settings`.`setting` = 'dbversion';
 
@@ -17,8 +18,9 @@ COMMIT;
 START TRANSACTION;
 
 ALTER TABLE `email_templates`
-  DROP COLUMN `emailTo`,
-  DROP COLUMN `emailCC`;
+    DROP COLUMN  `name`,
+    DROP COLUMN `emailTo`,
+    DROP COLUMN `emailCC`;
 
 COMMIT;
 
