5be7e3a6f99e720fb6d1dcb99c4c78cefed30d7f
Gregory Steinhoff <26551882+gsteinhoff@users.noreply.github.com>
2021-06-15 08:21:48 -0500

    LEAF-2139 - Changed setting update to correct date

--- LEAF_Request_Portal/utils/db_upgrade/Update_RMC_DB_2021041900-2021062800.sql
+++ LEAF_Request_Portal/utils/db_upgrade/Update_RMC_DB_2021041900-2021062800.sql
@@ -3,7 +3,7 @@ START TRANSACTION;
 INSERT INTO `email_templates` (emailTemplateID, label, emailTo, emailCc, subject, body)
 VALUES (-4, 'Mass Action Email Reminder Template', 'LEAF_mass_action_remind_emailTo.tpl', 'LEAF_mass_action_remind_emailCc.tpl', 'LEAF_mass_action_remind_subject.tpl', 'LEAF_mass_action_remind_body.tpl');
 
-UPDATE `settings` SET `data` = '2021061500' WHERE `settings`.`setting` = 'dbversion';
+UPDATE `settings` SET `data` = '2021062800' WHERE `settings`.`setting` = 'dbversion';
 
 COMMIT;
 
