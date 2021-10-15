713429345831d816e5b8e9c24bb5bfc78ec64a3e
Pete Nerantzinis <17956374+pete-nerantzinis@users.noreply.github.com>
2021-10-15 13:36:11 -0400

    LEAF-2540: Add fastdata INDEX

--- LEAF_Request_Portal/utils/db_upgrade/Update_RMC_DB_2021091600-2021101400.sql
+++ LEAF_Request_Portal/utils/db_upgrade/Update_RMC_DB_2021091600-2021101400.sql
@@ -2,6 +2,7 @@ START TRANSACTION;
 
 CREATE INDEX `userID` on `records` (`userID`);
 CREATE INDEX `submitted` on `records` (`submitted`);
+CREATE INDEX `fastdata` on `data` (`indicatorID`, `data`(10));
 
 UPDATE `settings` SET `data` = '2021101400' WHERE `settings`.`setting` = 'dbversion';
 
@@ -11,7 +12,9 @@ COMMIT;
 
 START TRANSACTION;
 
-
+DROP INDEX `userID` on `records`;
+DROP INDEX `submitted` on `records`;
+DROP INDEX `fastdata` on `data`;
 
 UPDATE `settings` SET `data` = '2021091600' WHERE `settings`.`setting` = 'dbversion';
 
