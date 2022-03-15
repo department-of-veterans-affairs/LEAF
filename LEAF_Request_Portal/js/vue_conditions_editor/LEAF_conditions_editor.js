4f392f945b7bda9ebe8e70c8dcf2fa23dea02480
Carrie Hanscom <chanscom7@gmail.com>
2022-03-15 11:35:16 -0400

    LEAF-2752 initial form list organization

--- LEAF_Request_Portal/js/vue_conditions_editor/LEAF_conditions_editor.js
+++ LEAF_Request_Portal/js/vue_conditions_editor/LEAF_conditions_editor.js
@@ -2,14 +2,53 @@ const ConditionsEditor = Vue.createApp({
     data() {
         return {
             forms: [],
-            selectedFormCatID: ''
+            selectedFormCatID: '',
+            selectedFormIndicators: []
         }
     },
     methods: {
+        getCategoryIndicators(catID) {
+            this.selectedFormCatID = catID;
+            //update selectedFormCatID and get indicators for list
+            //*
+            const xhttp = new XMLHttpRequest();
+            let formStructure = {
+                categoryID: catID,
+                indicators: [],
+                internalForms: {},
+            };
+            xhttp.onreadystatechange = () => {
+                if (xhttp.readyState == 4 && xhttp.status == 200) {
+                    const indicatorList = JSON.parse(xhttp.responseText);
+                    //
+                    const filteredList = indicatorList.filter(form => form.categoryID === catID || form.parentCategoryID === catID);
+                    this.selectedFormIndicators = filteredList;
+                    console.log(filteredList);
+                    //object that better represents form structure
+                    filteredList.forEach(indicator => {
+                        //internal forms
+                        if (indicator.parentCategoryID === catID) {
+                            const internalCatID = indicator.categoryID;
+                            if (typeof formStructure.internalForms[internalCatID] !== "undefined") {
+                                formStructure.internalForms[internalCatID].push({...indicator});
+                            } else {
+                                formStructure.internalForms = {...formStructure.internalForms, [internalCatID]: [{...indicator}]};
+                            }
+                        } else {
+                            formStructure.indicators.push({...indicator});
+                        }
+                    });
+                    console.log(formStructure);
+                }
+            };
+            xhttp.open("GET", `../api/form/indicator/list`, true);
+            xhttp.send(); //*/
+        }
     },
     beforeMount(){
         //get forms for dropdown
-        let xhttp = new XMLHttpRequest();
+        //* NOTE: xml
+        const xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = () => {
             if (xhttp.readyState == 4 && xhttp.status == 200) {
                 const list = JSON.parse(xhttp.responseText);
@@ -18,11 +57,27 @@ const ConditionsEditor = Vue.createApp({
             }
         };
         xhttp.open("GET", "../api/form/categories", true);
-        xhttp.send();
+        xhttp.send(); //*/
+        /* //NOTE: fetch API
+        fetch("../api/form/categories").then(response => { 
+                const contentType = response.headers.get('content-type');
+                if (!contentType || !contentType.includes("application/json")) {
+                    throw new TypeError("returned content-type not JSON");
+                }
+                if (response.status !== 200) {
+                    console.log('status:', response.status, response.statusText);
+                } else return response.json();
+            }).then(data => {
+                const filteredData = data.filter(ele => ele.categoryID.includes('form_'));
+                this.forms = filteredData.sort((a,b) => a.categoryName - b.categoryName).slice();
+            }).catch(err => console.log(err));*/
     },
     template: `<div>
         <div id="condition_editor_content">
-            <editor-nav :forms="forms"></editor-nav>
+            <editor-list :forms="forms" 
+                :selectedIndicators="selectedFormIndicators"
+                @update-selected-form="getCategoryIndicators">
+            </editor-list>
             <editor-main></editor-main>
             <editor-actions></editor-actions>
         </div>
@@ -30,31 +85,30 @@ const ConditionsEditor = Vue.createApp({
 });
 
 
-ConditionsEditor.component('editor-nav', {
+ConditionsEditor.component('editor-list', {
     data() {
         return {
             selectedCategoryID: ''
         }
     },
     props: {
-        forms: Array
+        forms: Array,
+        selectedIndicators: Array
     },
     methods: {
         selectForm(){
-            //TODO:
+            this.$emit('update-selected-form', this.selectedCategoryID);
         }
     },
-    template: `<div id="condition_editor_nav">
+    template: `<div id="condition_editor_list">
         <h3>Conditions List</h3>
-        <!-- TODO: emit to parent for other components -->
-        <select title="select a form" name="form-selector" v-model="selectedCategoryID"> 
+        <select title="select a form" name="form-selector" v-model="selectedCategoryID" @change="selectForm"> 
             <option v-for="f in forms" :title="f.categoryName" :value="f.categoryID">{{f.categoryName}}</option>
         </select>
-        <p>selected catID: {{ selectedCategoryID }}</p>
+        <i class="TEST"><p>selected catID: {{ selectedCategoryID }}</p>
+        <p>indicator info (placeholder)</p></i>
         <ul>
-            <li>temp</li>
-            <li>list</li>
-            <li>items</li>
+            <li v-for="i in selectedIndicators">{{ i.name }} (indicator {{ i.indicatorID }})</li>
         </ul>
     </div>`
 });
