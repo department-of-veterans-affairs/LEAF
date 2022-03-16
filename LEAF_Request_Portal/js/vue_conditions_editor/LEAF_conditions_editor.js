fea77b9512fd16fb7722d38ddc916710189e689a
Carrie Hanscom <chanscom7@gmail.com>
2022-03-16 12:50:51 -0400

    LEAF-2752 value options for some formats, child indicator selector

--- LEAF_Request_Portal/js/vue_conditions_editor/LEAF_conditions_editor.js
+++ LEAF_Request_Portal/js/vue_conditions_editor/LEAF_conditions_editor.js
@@ -6,50 +6,68 @@ const ConditionsEditor = Vue.createApp({
             selectedIndicator: {},
             selectedFormIndicators: [],
             selectedFormConditions: [],
-            selectedOperators: [],
+            selectedParentOperators: [],
             selectedFormat: '',
-            formStructure: {} //TEST
+            selectedValueOptions: [], //for radio, dropdown
+            childIndicator: {},
+            childIndicatorOptions: [],  //selectedform inds - selected ind
+            formStructure: {} //TEST, TODO:
         }
     },
     methods: {
         updateSelectedIndicator(indicator){
             this.selectedIndicator = indicator;
-            this.selectedOperators = [];
+            this.selectedParentOperators = [];
+            this.selectedValueOptions = [];
+            this.childIndicator = {};
+            this.childIndicatorOptions = this.selectedFormIndicators.filter(i => i.indicatorID !== indicator.indicatorID);
+
             this.format = indicator.format.indexOf("\n") === -1 ?
                          indicator.format : indicator.format.substr(0, indicator.format.indexOf("\n")).trim();
-
+            this.selectedValueOptions = indicator.format.indexOf("\n") === -1 ?
+                         [] : indicator.format.slice(indicator.format.indexOf("\n")+1).split("\n");
+            console.log(indicator.format, 'val options', this.selectedValueOptions);             
             switch(this.format) {
                 case 'number':
-                case 'currency':  //NOTE: currency and number do not have != option in formSearch.js - why? vals are otherwise the same, but text differs
+                case 'currency':
+                    this.selectedParentOperators = [
+                        {val:"==", text: "is equal to"}, 
+                        {val:"!=", text: "is not equal to"},
+                        {val:">", text: "is greater than"},
+                        {val:">=", text: "is greater than or equal to"},
+                        {val:"<", text: "is less than"},
+                        {val:"<=",text: "is less than or equal to"}
+                    ];
+                    break;
                 case 'multiselect':
                 case 'dropdown':
-                case 'radio':      
-                    this.selectedOperators = [
-                        {val:"=", text:"IS"}, 
-                        {val:"!=", text:"IS NOT"},
-                        {val:">", text:">" },
-                        {val:">=", text: ">="},
-                        {val:"<", text: "<"},
-                        {val:"<=",text: "<="}
+                case 'radio':
+                    this.selectedParentOperators = [
+                        {val:"==", text: "selected value(s) is"}, 
+                        {val:"!=", text: "selected value(s) is not"}
                     ];
                     break;
-                case 'date': //TODO: does back handle type=date inputs?
-                    this.selectedOperators = [
-                        {val:"=", text: "ON"}, 
-                        {val:">=", text:"ON AND AFTER"},
-                        {val:"<=", text:"ON AND BEFORE" }
+                case 'checkbox':
+                    this.selectedParentOperators = [
+                        {val:"==", text: "is checked"}, 
+                        {val:"!=", text: "is not checked"}
+                    ];
+                    break;          
+                case 'date':
+                    this.selectedParentOperators = [
+                        {val:"==", text: "on"}, 
+                        {val:">=", text: "on and after"},
+                        {val:"<=", text: "on and before"}
                     ];
                     break;
-                case 'orgchart_employee':
+                case 'orgchart_employee': //NOTE: orgchart formats are currently excluded from indicator selection
                 case 'orgchart_group':  
                 case 'orgchart_position':
                     break;  
                 default:
-                    this.selectedOperators = [
-                        {val:"LIKE", text: "CONTAINS"}, 
-                        {val:"NOT LIKE", text:"DOES NOT CONTAIN"},
-                        {val:"=", text:"=" },
-                        {val:"!=", text:"!=" }
+                    this.selectedParentOperators = [
+                        {val:"LIKE", text: "contains"}, 
+                        {val:"NOT LIKE", text:"does not contain"}
                     ]; 
                     break;
             }
@@ -58,9 +76,11 @@ const ConditionsEditor = Vue.createApp({
             //update catID and clear potential prev indicator selection
             this.selectedFormCatID = catID;
             this.selectedIndicator = {};
-            this.selectedOperators = [];
+            this.selectedParentOperators = [];
+            this.selectedValueOptions = [];  //values for radio, multiselect, dropdown formats
+            this.childIndicatorOptions = []; //which child indicators can be selected
  
-            let formStructure = { //TODO: better list menu 
+            let formStructure = { //TEST, TODO: better list menu 
                 categoryID: catID,
                 indicators: [],
                 internalForms: {},
@@ -71,13 +91,13 @@ const ConditionsEditor = Vue.createApp({
                 if (xhttp.readyState == 4 && xhttp.status == 200) {
                     const indicatorList = JSON.parse(xhttp.responseText);
                     //all indicators associated with this form, for dropdown selection to add conditions
-                    const formIndicators = indicatorList.filter(indi => indi.categoryID === catID || indi.parentCategoryID === catID);
+                    const formIndicators = indicatorList.filter(indi => !indi.format.includes('orgchart') && (indi.categoryID === catID || indi.parentCategoryID === catID));
                     //indicators that already have conditions, for list display / edit
-                    const formConditions = formIndicators.filter(indi => indi.condition !== null);
+                    const formConditions = formIndicators.filter(indi => indi.condition !== null && indi.condition !== '');
                     this.selectedFormIndicators = formIndicators;
                     this.selectedFormConditions = formConditions;
                     
-                    //object that better represents form structure NOTE: not currently used
+                    //object that better represents form structure TEST, TODO: NOTE: not currently used
                     formIndicators.forEach(indicator => {
                         //internal forms
                         if (indicator.parentCategoryID === catID) {
@@ -132,26 +152,30 @@ const ConditionsEditor = Vue.createApp({
                 :selectedConditions="selectedFormConditions"
                 @update-selected-form="getCategoryIndicators">
             </editor-list>
-            <editor-main 
+            <editor-main
+                :selectedValueOptions="selectedValueOptions" 
                 :selectedIndicators="selectedFormIndicators"
                 :selectedIndicatorProp="selectedIndicator"
-                :selectedOperators="selectedOperators"
+                :selectedParentOperators="selectedParentOperators"
+                :childIndicatorOptions="childIndicatorOptions"
                 @update-selected-indicator="updateSelectedIndicator">
             </editor-main>
             <editor-actions></editor-actions>
         </div>
 
         <div class="TEST">
-            <p>selected catID: {{ selectedFormCatID }}</p>
-            <p>selected indID: {{ selectedIndicator }}</p>
-            <p>indicator info for selected form: {{ selectedFormIndicators }}</p>
-            <p>indicators that have conditions: {{ selectedFormConditions }}</p>
+            <p><b>selected catID:</b> {{ selectedFormCatID }}</p>
+            <p><b>selected indID:</b> {{ selectedIndicator }}</p>
+            <p><b>indicator info for selected form:</b> {{ selectedFormIndicators }}</p>
+            <p><b>indicators that have conditions:</b> {{ selectedFormConditions }}</p>
         </div>
     </div>`
 });
 
 
-//LIST COMPONENT Allows form selection and shows indicators with conditions for selected form
+//LIST COMPONENT 
+//Allows form selection and shows indicators with conditions for selected form
+//TODO: tab indicators with conditions to view details and/or edit
 ConditionsEditor.component('editor-list', {
     data() {
         return {
@@ -184,35 +208,68 @@ ConditionsEditor.component('editor-list', {
 ConditionsEditor.component('editor-main', {
     data() {
         return {
-            selectedIndicator: {}
+            selectedIndicator: {},
+            selectedChildIndicator: {}
         }
     },
     props: {
-        selectedIndicators: Array,
-        selectedIndicatorProp: Object,
-        selectedOperators: Array
+        selectedIndicators: Array,      //all available inds for currently selected form
+        selectedIndicatorProp: Object,  //info for the currently selected indicator
+        selectedParentOperators: Array,       //available operators for the format of above ind
+        selectedValueOptions: Array,    //values for dropdown formats
+        childIndicator: Object,
+        childIndicatorOptions: Array, //which indicators can be chosen for children
     },
     methods: {
         selectIndicator() {
             this.$emit('update-selected-indicator', this.selectedIndicator);
-        }
+        },
+        selectChildIndicator() {
+            this.$emit('update-selected-child', this.selectedChildIndicator);
+        },
+        validateCurrency(event){
+            const currencyRegex = /^(\d*)(\.\d{0,2})?$/;
+            const val = event.target.value;
+            if (!currencyRegex.test(val)) { //TODO: userfeedback
+                document.getElementById('currency-format-input').value = '';
+            }
+        },
     },
     template: `<div id="condition_editor_main">
         <h3>Conditions Editor</h3>
         <div v-if="selectedIndicators.length > 0">
             <h4>IF</h4>
-            <p>Parent Question Indicator</p>
+            <span>Parent Question Indicator</span>
             <select title="select an indicator" 
                     name="indicator-selector" 
                     v-model="selectedIndicator" 
                     @change="selectIndicator">    
                 <option v-for="i in selectedIndicators" :title="i.name" :value="i">{{i.name }} (indicator {{i.indicatorID}})</option>
             </select>
-            <div class="TEST">operators based on format: {{ selectedIndicatorProp.format }}</div>
-            <select v-if="selectedOperators.length > 0">
-                <option v-for="o in selectedOperators" :value="o.val">{{ o.text }}</option>
+            <select v-if="selectedParentOperators.length > 0">
+                <option v-for="o in selectedParentOperators" :value="o.val">{{ o.text }}</option>
             </select>
             <input v-if="selectedIndicatorProp.format==='date'" type="date"/>
+            <input v-if="selectedIndicatorProp.format==='number'" type="number"/>
+            <input v-if="selectedIndicatorProp.format==='currency'"
+                id="currency-format-input" 
+                type="number" step="0.01" @change="validateCurrency"/>
+            <select v-if="typeof selectedIndicatorProp.format === 'string' 
+                && selectedIndicatorProp.format.includes('dropdown')">
+                <option v-for="val in selectedValueOptions"> {{ val }} </option>
+            </select>
+            <select v-if="typeof selectedIndicatorProp.format === 'string' 
+            && selectedIndicatorProp.format.includes('radio')">
+                <option v-for="val in selectedValueOptions"> {{ val }} </option>
+            </select>     
+            <h4>THEN</h4>
+            <span>Child Question Indicator</span>
+            <select title="select an indicator" 
+                    name="child-indicator-selector" 
+                    v-model="selectedChildIndicator" 
+                    @change="selectChildIndicator">    
+                <option v-for="c in childIndicatorOptions" :title="c.name" :value="c">{{c.name }} (indicator {{c.indicatorID}})</option>
+            </select>
         </div>
     </div>`
 });
