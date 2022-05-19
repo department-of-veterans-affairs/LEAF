825377e8bc9a230c2f470b8f9082f51496a69286
Carrie Hanscom <chanscom7@gmail.com>
2022-05-19 09:17:29 -0400

    LEAF 2834 update PDF print multicheck for array

--- LEAF_Request_Portal/js/formPrint.js
+++ LEAF_Request_Portal/js/formPrint.js
@@ -485,8 +485,7 @@ var printer = function() {
                                     }
                                     doc.rect(horizontalShift, verticalShift + 6, 5, 5);
                                     //make selected values consistently arrays for checkboxes, multisel, radio, checkbox, dropdown
-                                    let selectedVals = indicator.format === 'checkboxes' ? indicator.value.slice() 
-                                        : indicator.format === 'multiselect' ? indicator.value.split(/,(?!\s)/) : [indicator.value];
+                                    let selectedVals = indicator.format === 'checkboxes' || indicator.format === 'multiselect' ? indicator.value.slice() : [indicator.value];
                                     selectedVals = selectedVals.filter(v => v !== '');
                                     if (!blank && selectedVals.indexOf(indicator.options[i]) > -1) {
                                         doc.text('x', horizontalShift + 1.5, verticalShift + 9.5);
