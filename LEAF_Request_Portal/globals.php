2d8659ce08106e3e2a9c30ee52d1377871992118
Pete Nerantzinis <17956374+pete-nerantzinis@users.noreply.github.com>
2022-08-16 10:45:44 -0400

    Update globals.php

--- LEAF_Request_Portal/globals.php
+++ LEAF_Request_Portal/globals.php
@@ -1,17 +1,16 @@
 <?php
 if (!defined('PRODUCT_NAME')) define('PRODUCT_NAME', getenv('PRODUCT_NAME'));
-if (!defined('PORTAL_VERSION_NUMBER')) define('PORTAL_VERSION_NUMBER', getenv('PORTAL_VERSION_NUMBER'));
+if (!defined('VERSION_NUMBER')) define('VERSION_NUMBER', getenv('PORTAL_VERSION_NUMBER'));
 
 if (!defined('DATABASE_DB_ADMIN')) define('DATABASE_DB_ADMIN', getenv('DATABASE_DB_ADMIN'));
 
 if (!defined('DIRECTORY_HOST')) define('DIRECTORY_HOST', getenv('DATABASE_HOST'));
-if (!defined('DIRECTORY_DB')) define('DIRECTORY_DB', getenv('NEXUS_DB'));
-if (!defined('PORTAL_DB')) define('PORTAL_DB', getenv('PORTAL_DB'));
+if (!defined('DIRECTORY_DB')) define('DIRECTORY_DB', getenv('DATABASE_DB_DIRECTORY'));
 if (!defined('DIRECTORY_USER')) define('DIRECTORY_USER', getenv('DATABASE_USERNAME'));
 if (!defined('DIRECTORY_PASS')) define('DIRECTORY_PASS', getenv('DATABASE_PASSWORD'));
 
 if (!defined('LEAF_NEXUS_URL')) define('LEAF_NEXUS_URL', getenv('APP_URL_NEXUS'));
 if (!defined('HTTP_HOST')) define('HTTP_HOST', getenv('APP_HTTP_HOST'));
-if (!defined('PORTAL_AUTH_URL')) define('PORTAL_AUTH_URL', getenv('APP_PORTAL_URL_AUTH'));
+if (!defined('AUTH_URL')) define('AUTH_URL', getenv('APP_URL_AUTH'));
 if (!defined('AUTH_TYPE')) define('AUTH_TYPE', getenv('APP_AUTH_TYPE'));
 if (!defined('CIPHER_KEY')) define('CIPHER_KEY', getenv('APP_CIPHER_KEY'));
