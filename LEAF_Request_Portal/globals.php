ddd00706109794f7066856a6d2420b10dc6d7255
Carrie <chanscom7@gmail.com>
2022-09-01 15:48:39 -0400

    update configs

--- LEAF_Request_Portal/globals.php
+++ LEAF_Request_Portal/globals.php
@@ -1,14 +1,11 @@
 <?php
 if (!defined('PRODUCT_NAME')) define('PRODUCT_NAME', getenv('PRODUCT_NAME'));
 if (!defined('VERSION_NUMBER')) define('VERSION_NUMBER', getenv('PORTAL_VERSION_NUMBER'));
-
 if (!defined('DATABASE_DB_ADMIN')) define('DATABASE_DB_ADMIN', getenv('DATABASE_DB_ADMIN'));
-
 if (!defined('DIRECTORY_HOST')) define('DIRECTORY_HOST', getenv('DATABASE_HOST'));
 if (!defined('DIRECTORY_DB')) define('DIRECTORY_DB', getenv('DATABASE_DB_DIRECTORY'));
 if (!defined('DIRECTORY_USER')) define('DIRECTORY_USER', getenv('DATABASE_USERNAME'));
 if (!defined('DIRECTORY_PASS')) define('DIRECTORY_PASS', getenv('DATABASE_PASSWORD'));
-
 if (!defined('LEAF_NEXUS_URL')) define('LEAF_NEXUS_URL', getenv('APP_URL_NEXUS'));
 if (!defined('HTTP_HOST')) define('HTTP_HOST', getenv('APP_HTTP_HOST'));
 if (!defined('AUTH_URL')) define('AUTH_URL', getenv('APP_URL_AUTH'));
