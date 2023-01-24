ba298baa403d0caeaf95c26d8a1f294b9adaa69b
Jamie P Holcomb <jamie.holcomb@adaptivestack.com>
2023-01-24 09:50:19 -0500

    Leaf 3427 add dbversion and format update properly

--- docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2022110300-2023012400.sql
+++ docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2022110300-2023012400.sql
@@ -1,7 +1,9 @@
 START TRANSACTION;
 
-UPDATE `indicators` SET `html` = '2022110300' WHERE `indicators`.`html` = `<div id=\"leafSecureDialogContent\"></div>\n\n<script src=\"js/LeafSecureReviewDialog.js\"></script>\n<script>\n$(function() {\n\n    LeafSecureReviewDialog(\'leafSecureDialogContent\');\n\n});\n</script>`;
-UPDATE `indicators` SET `html` = '2022110300' WHERE `indicators`.`htmlPrint` = `<div id=\"leafSecureDialogContentPrint\"></div>\n\n<script src=\"js/LeafSecureReviewDialog.js\"></script>\n<script>\n$(function() {\n\n    LeafSecureReviewDialog(\'leafSecureDialogContentPrint\');\n\n});\n</script>`;
+UPDATE `indicators` SET `html` = '<div id=\"leafSecureDialogContent\"></div>\n\n<script src=\"js/LeafSecureReviewDialog.js\"></script>\n<script>\n$(function() {\n\n    LeafSecureReviewDialog(\'leafSecureDialogContent\');\n\n});\n</script>' WHERE `indicators`.`indicatorID` = `-2` ;
+UPDATE `indicators` SET `htmlPrint` = '<div id=\"leafSecureDialogContentPrint\"></div>\n\n<script src=\"js/LeafSecureReviewDialog.js\"></script>\n<script>\n$(function() {\n\n    LeafSecureReviewDialog(\'leafSecureDialogContentPrint\');\n\n});\n</script>' WHERE `indicators`.`indicatorID` = `-2`;
+
+UPDATE `settings` SET `data` = '2023012400' WHERE `settings`.`setting` = 'dbversion';
 
 COMMIT;
 
