01f2a52c6881ae40fdc666ff424e981ff04872d0
Jamie P Holcomb <jamie.holcomb@adaptivestack.com>
2023-01-24 10:09:47 -0500

    Leaf 3427 update revert data

--- docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2022110300-2023012400.sql
+++ docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2022110300-2023012400.sql
@@ -11,13 +11,10 @@ COMMIT;
 
 START TRANSACTION;
 
-ALTER TABLE `workflow_steps` DROP COLUMN `stepData`;
+UPDATE `indicators` SET `html` = '<div id=\"leafSecureDialogContent\"></div>\n\n<script src=\"js/LeafSecureReviewDialog.js\" />\n<script>\n$(function() {\n\n    LeafSecureReviewDialog(\'leafSecureDialogContent\');\n\n});\n</script>' WHERE `indicators`.`indicatorID` = -2;
+UPDATE `indicators` SET `htmlPrint` = '<div id=\"leafSecureDialogContentPrint\"></div>\n\n<script src=\"js/LeafSecureReviewDialog.js\" />\n<script>\n$(function() {\n\n    LeafSecureReviewDialog(\'leafSecureDialogContentPrint\');\n\n});\n</script>' WHERE `indicators`.`indicatorID` = -2;
 
-DELETE FROM `email_templates` WHERE  `emailTemplateID`=-5;
-
-ALTER TABLE `records_workflow_state` DROP COLUMN `lastNotified`;
-
-UPDATE `settings` SET `data` = '2022050300' WHERE `settings`.`setting` = 'dbversion';
+UPDATE `settings` SET `data` = '2022110300' WHERE `settings`.`setting` = 'dbversion';
 
 
 COMMIT;
