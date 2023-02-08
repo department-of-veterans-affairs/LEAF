609f32884e6158c23d3cc321eddd332e06be53fa
Juan Rodriguez <info@jrodcode.com>
2023-02-08 13:01:24 -0800

    LEAF-2875 Mark old accounts as disabled
    This will make sure that if the user hasn't been updated after one week to be marked as disabled by adding the timestamp.

--- LEAF_Nexus/scripts/refreshOrgchartEmployees.php
+++ LEAF_Nexus/scripts/refreshOrgchartEmployees.php
@@ -31,81 +31,114 @@ if (strtolower($config->dbName) == strtolower(DIRECTORY_DB)) {
     echo 1; // success value
 } else {
 
-    if(!empty($_GET['userName']) && !empty($_GET['empUID'])){
+    if (!empty($_GET['userName']) && !empty($_GET['empUID'])) {
         updateUserInfo($_GET['userName'], $_GET['empUID']);
         echo 1;
-    }else{
+    } else {
 
         $startTime = time();
         // echo "Refresh Orgchart Employees Start\n";
-
         updateLocalOrgchartBatch();
 
         $endTime = time();
         // echo "Refresh Complete!\nCompletion time: " . date("U.v", $endTime-$startTime) . " seconds";
         echo 1; // success value
     }
-
 }
 
 /*
  *	Updates single employee information from national orgchart to local orgchart
 */
-function updateUserInfo($userName, $empUID){
-	global $db, $phonedb;
+function updateUserInfo($userName, $empUID)
+{
+    global $db, $phonedb;
 
-	$vars = array(':userName' => htmlspecialchars_decode($userName, ENT_QUOTES)); //for users with apostrophe in name
+    $vars = array(':userName' => htmlspecialchars_decode($userName, ENT_QUOTES)); //for users with apostrophe in name
 
-	$sql = "SELECT empUID, userName, lastName, firstName, middleName, phoneticLastName, phoneticFirstName, domain, deleted, lastUpdated
+    $sql = "SELECT empUID, userName, lastName,
+            firstName, middleName, phoneticLastName,
+            phoneticFirstName, domain, deleted, lastUpdated
 			FROM employee
 			WHERE userName=:userName";
 
-	$sql2 = "UPDATE employee
-		SET lastName=:lastName,
-		firstName=:firstName,
-		middleName=:midInit,
-		phoneticFirstName=:phoneticFname,
-		phoneticLastName=:phoneticLname,
-		domain=:domain,
-		deleted=:deleted,
-		lastUpdated=:lastUpdated
-		WHERE userName=:userName";
+    $sql2 = "UPDATE employee
+            SET lastName=:lastName,
+            firstName=:firstName,
+            middleName=:midInit,
+            phoneticFirstName=:phoneticFname,
+            phoneticLastName=:phoneticLname,
+            domain=:domain,
+            deleted=:deleted,
+            lastUpdated=:lastUpdated
+            WHERE userName=:userName";
 
-	#used to disable if not found in national
+    #used to disable if not found in national
     $sql3 = "UPDATE employee
-        SET deleted=:deleted
-        WHERE userName=:userName";
+            SET deleted=:deleted
+            WHERE userName=:userName";
 
-	$res = $phonedb->prepared_query($sql, $vars);
+    $res = $phonedb->prepared_query($sql, $vars);
 
-	if (count($res) == 0){
-	    //if there is no record in nat, disable the account.
-	    $vars = array(
-	        ':userName' => $userName,
+    if (count($res) == 0) {
+        //if there is no record in nat, disable the account.
+        $vars = array(
+            ':userName' => $userName,
             ':deleted' => time()
         );
-	    $db->prepared_query($sql3, $vars);
+        $db->prepared_query($sql3, $vars);
     }
 
-	if (count($res) > 0) {
-		$vars = array(
-				':userName' => $res[0]['userName'],
-				':lastName' => $res[0]['lastName'],
-				':firstName' => $res[0]['firstName'],
-				':midInit' => $res[0]['middleName'],
-				':phoneticFname' => $res[0]['phoneticFirstName'],
-				':phoneticLname' => $res[0]['phoneticLastName'],
-				':domain' => $res[0]['domain'],
-				':deleted' => $res[0]['deleted'],
-				':lastUpdated' => $res[0]['lastUpdated']
-		);
+    if (count($res) > 0) {
+        $vars = array(
+            ':userName' => $res[0]['userName'],
+            ':lastName' => $res[0]['lastName'],
+            ':firstName' => $res[0]['firstName'],
+            ':midInit' => $res[0]['middleName'],
+            ':phoneticFname' => $res[0]['phoneticFirstName'],
+            ':phoneticLname' => $res[0]['phoneticLastName'],
+            ':domain' => $res[0]['domain'],
+            ':deleted' => $res[0]['deleted'],
+            ':lastUpdated' => $res[0]['lastUpdated']
+        );
 
-		// sets local employee table
-		$db->prepared_query($sql2, $vars);
+        // sets local employee table
+        $db->prepared_query($sql2, $vars);
 
-		// sets local employee_data table
-		updateEmployeeData($res[0]['empUID'], $empUID);
-	}
+        // sets local employee_data table
+        updateEmployeeData($res[0]['empUID'], $empUID);
+    }
+
+
+    if (count($res) > 0) {
+        $currentTime = time();
+        $oneWeekAdded = $res[0]['lastUpdated'] + 604800;
+
+        if ($currentTime > $oneWeekAdded) {
+            // user is disabled if userUpdated is lower than the current time
+            $vars = array(
+                ':deleted' => $currentTime,
+                ':userName' => $userName
+            );
+            $sql = "UPDATE employee
+                    SET userDisabled=:deleted
+                    WHERE userName=:userName";
+
+            $db->prepared_query($sql, $vars);
+        }
+
+        // This will ensure that the user is enabled UserUpdated is 0
+        if ($res[0]['lastUpdated'] == 0) {
+            $vars = array(
+                ':deleted' => 0,
+                ':userName' => $userName
+            );
+            $sql2 = "UPDATE employee
+                     SET deleted=:deleted
+                     WHERE userName=:userName";
+
+            $db->prepared_query($sql2, $vars);
+        }
+    }
 }
 
 function updateLocalOrgchartBatch()
@@ -159,21 +192,21 @@ function updateEmployeeDataBatch(array $localEmployeeUsernames = [])
     // get org employees
     $orgEmployeeSql = "SELECT empUID, userName, lastName, firstName, middleName, phoneticLastName, phoneticFirstName, domain, deleted, lastUpdated
     		FROM employee
-    		WHERE userName IN (".implode(",",array_fill(1, count($localEmployeeUsernames), '?')).")";
+    		WHERE userName IN (" . implode(",", array_fill(1, count($localEmployeeUsernames), '?')) . ")";
 
-    $orgEmployeeRes = $phonedb->prepared_query($orgEmployeeSql,$localEmployeeUsernames);
-	
+    $orgEmployeeRes = $phonedb->prepared_query($orgEmployeeSql, $localEmployeeUsernames);
+
     // get local empuids
-    $localEmployeeSql = "SELECT empUID, userName FROM employee WHERE userName IN (".implode(",",array_fill(1, count($localEmployeeUsernames), '?')).")";
-    $localEmpUIDs = $db->prepared_query($localEmployeeSql,$localEmployeeUsernames);
+    $localEmployeeSql = "SELECT empUID, userName FROM employee WHERE userName IN (" . implode(",", array_fill(1, count($localEmployeeUsernames), '?')) . ")";
+    $localEmpUIDs = $db->prepared_query($localEmployeeSql, $localEmployeeUsernames);
 
     $localEmpArray = [];
     foreach ($localEmpUIDs as $localUsername) {
         $localEmpArray[$localUsername['userName']] = $localUsername['empUID'];
     }
-	
+
     //if for some reason there is no data, we need to stop right there.
-    if(empty($orgEmployeeRes)){
+    if (empty($orgEmployeeRes)) {
         return FALSE;
     }
     foreach ($orgEmployeeRes as $orgEmployee) {
@@ -191,25 +224,24 @@ function updateEmployeeDataBatch(array $localEmployeeUsernames = [])
             'deleted' => $orgEmployee['deleted'],
             'lastUpdated' => $orgEmployee['lastUpdated']
         ];
-
     }
-	
+
     $localDeletedEmployees = array_diff(array_column($localEmpUIDs, 'userName'), array_column($orgEmployeeRes, 'userName'));
-    $deletedEmployeesSql = "UPDATE employee SET deleted=UNIX_TIMESTAMP(NOW()) WHERE userName IN (".implode(",",array_fill(1, count($localDeletedEmployees), '?')).")";
+    $deletedEmployeesSql = "UPDATE employee SET deleted=UNIX_TIMESTAMP(NOW()) WHERE userName IN (" . implode(",", array_fill(1, count($localDeletedEmployees), '?')) . ")";
 
     if (!empty($localDeletedEmployees)) {
-        $db->prepared_query($deletedEmployeesSql,array_values($localDeletedEmployees));
+        $db->prepared_query($deletedEmployeesSql, array_values($localDeletedEmployees));
     }
 
-    $db->insert_batch('employee',$localEmployeeArray,['lastName','firstName','middleName','phoneticFirstName','phoneticLastName','domain','deleted','lastUpdated']);
+    $db->insert_batch('employee', $localEmployeeArray, ['lastName', 'firstName', 'middleName', 'phoneticFirstName', 'phoneticLastName', 'domain', 'deleted', 'lastUpdated']);
 
     // STEP 2: Get employee_data updated
     // get the employee data, we will need to get the employee ids first
 
-    $orgEmployeeDataSql = "SELECT empUID, employee.userName, indicatorID, data, author, timestamp 
-    				FROM employee_data 
-				LEFT JOIN employee USING (empUID) 
-				WHERE empUID IN ('".implode("','", $nationalEmpUIDs)."') AND indicatorID IN (:PHONEIID,:EMAILIID,:LOCATIONIID,:ADTITLEIID)";
+    $orgEmployeeDataSql = "SELECT empUID, employee.userName, indicatorID, data, author, timestamp
+    				FROM employee_data
+				LEFT JOIN employee USING (empUID)
+				WHERE empUID IN ('" . implode("','", $nationalEmpUIDs) . "') AND indicatorID IN (:PHONEIID,:EMAILIID,:LOCATIONIID,:ADTITLEIID)";
 
     $orgEmployeeDataVars = [
         ':PHONEIID' => PHONEIID,
@@ -220,23 +252,21 @@ function updateEmployeeDataBatch(array $localEmployeeUsernames = [])
 
     $orgEmployeeDataRes = $phonedb->prepared_query($orgEmployeeDataSql, $orgEmployeeDataVars);
 
-    if(empty($orgEmployeeDataRes)){
+    if (empty($orgEmployeeDataRes)) {
         return FALSE;
     }
-    foreach($orgEmployeeDataRes as $orgEmployeeData){
-	    
+    foreach ($orgEmployeeDataRes as $orgEmployeeData) {
+
         $localEmployeeDataArray[] = [
-                 'empUID' => $localEmpArray[$orgEmployeeData['userName']],
-                 'indicatorID' => $orgEmployeeData['indicatorID'],
-                 'data' => $orgEmployeeData['data'],
-                 'author' => $orgEmployeeData['author'],
-                 'timestamp' => $orgEmployeeData['timestamp'],
-             ];
+            'empUID' => $localEmpArray[$orgEmployeeData['userName']],
+            'indicatorID' => $orgEmployeeData['indicatorID'],
+            'data' => $orgEmployeeData['data'],
+            'author' => $orgEmployeeData['author'],
+            'timestamp' => $orgEmployeeData['timestamp'],
+        ];
     }
 
-    $db->insert_batch('employee_data',$localEmployeeDataArray,['indicatorID','data','author','timestamp']);
-
-
+    $db->insert_batch('employee_data', $localEmployeeDataArray, ['indicatorID', 'data', 'author', 'timestamp']);
 }
 
 /*
@@ -248,7 +278,11 @@ function updateEmployeeData($nationalEmpUID, $localEmpUID)
 {
     global $db, $phonedb;
 
-    $sql = "SELECT empUID, indicatorID, data, author, timestamp FROM employee_data WHERE empUID=:nationalEmpUID AND indicatorID IN (:PHONEIID,:EMAILIID,:LOCATIONIID,:ADTITLEIID)";
+    $sql = "SELECT empUID, indicatorID, data, author, timestamp
+            FROM employee_data
+            WHERE empUID=:nationalEmpUID
+            AND indicatorID
+            IN (:PHONEIID,:EMAILIID,:LOCATIONIID,:ADTITLEIID)";
 
     $selectVars = array(
         ':nationalEmpUID' => $nationalEmpUID,
@@ -263,10 +297,10 @@ function updateEmployeeData($nationalEmpUID, $localEmpUID)
     if (count($res) > 0) {
         for ($i = 0; $i < count($res); $i++) {
             $sql2 = "INSERT INTO employee_data (empUID, indicatorID, data, author, timestamp)
-				VALUES (:empUID, :indicatorID, :data, :author, :timestamp)
-				ON DUPLICATE KEY UPDATE data=:data,
-					author=:author,
-					timestamp=:timestamp";
+				     VALUES (:empUID, :indicatorID, :data, :author, :timestamp)
+				     ON DUPLICATE KEY UPDATE data=:data,
+					 author=:author,
+					 timestamp=:timestamp";
 
             $vars = array(
                 ':empUID' => $localEmpUID,
