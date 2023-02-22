8b7e1e34fb9a2c2758c3a86237389cc3f3961835
mgaoVA <16783916+mgaoVA@users.noreply.github.com>
2023-02-22 17:12:54 -0500

    align with HTTP specification for etags

--- LEAF_Request_Portal/api/RESTfulResponse.php
+++ LEAF_Request_Portal/api/RESTfulResponse.php
@@ -95,7 +95,7 @@ abstract class RESTfulResponse
 
                 if ($_SERVER['REQUEST_METHOD'] === 'GET')
                 {
-                    $etag = md5($jsonOut);
+                    $etag = '"' . md5($jsonOut) . '"';
                     header_remove('Pragma');
                     header_remove('Cache-Control');
                     header_remove('Expires');
