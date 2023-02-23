2390cbaa00770e923a77fe8ed95a181d04ed1a2c
Jamie P Holcomb <jamie.holcomb@adaptivestack.com>
2023-02-23 12:45:29 -0500

    Leaf 3529 for consistency sack

--- docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2023012400-2023022300.sql
+++ docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2023012400-2023022300.sql
@@ -1,6 +1,10 @@
 START TRANSACTION;
 
 ALTER TABLE notes MODIFY recordID mediumint(8) UNSIGNED NOT NULL;
+ALTER TABLE signatures MODIFY recordID mediumint(8) UNSIGNED NOT NULL;
+ALTER TABLE signatures MODIFY stepID smallint(6) UNSIGNED NOT NULL;
+ALTER TABLE signatures MODIFY dependencyID smallint(6) UNSIGNED NOT NULL;
+ALTER TABLE tags MODIFY recordID mediumint(8) UNSIGNED NOT NULL;
 
 UPDATE `settings` SET `data` = '2023022300' WHERE `settings`.`setting` = 'dbversion';
 
@@ -11,6 +15,10 @@ COMMIT;
 START TRANSACTION;
 
 ALTER TABLE notes MODIFY recordID mediumint(5) UNSIGNED NOT NULL;
+ALTER TABLE signatures MODIFY recordID smallint(5) UNSIGNED NOT NULL;
+ALTER TABLE signatures MODIFY stepID smallint(5) UNSIGNED NOT NULL;
+ALTER TABLE signatures MODIFY dependencyID smallint(5) UNSIGNED NOT NULL;
+ALTER TABLE tags MODIFY recordID int(11) UNSIGNED NOT NULL;
 
 UPDATE `settings` SET `data` = '2023012400' WHERE `settings`.`setting` = 'dbversion';
 
