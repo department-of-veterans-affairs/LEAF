ad1c9091bef9cc14ddd56678d61087c06fd07c1c
Shane O <shane.ottinger@adaptivestack.com>
2023-03-01 13:39:26 -0600

    LEAF-3346 - Add in the dbversion

--- docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2023012400-2023012600.sql
+++ docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2023012400-2023012600.sql
@@ -4,6 +4,8 @@ ALTER TABLE `records_workflow_state` ADD COLUMN `initialNotificationSent` TINYIN
 
 CREATE INDEX idx_lastNotified ON records_workflow_state (lastNotified);
 
+UPDATE `settings` SET `data` = '2023012600' WHERE `settings`.`setting` = 'dbversion';
+
 COMMIT;
 
 /**** Revert DB ****
@@ -13,6 +15,9 @@ START TRANSACTION;
 ALTER TABLE `records_workflow_state` DROP COLUMN `initialNotificationSent`;
 
 DROP INDEX idx_lastNotified ON records_workflow_state;
+
+UPDATE `settings` SET `data` = '2023012400' WHERE `settings`.`setting` = 'dbversion';
+
 
 COMMIT;
 
