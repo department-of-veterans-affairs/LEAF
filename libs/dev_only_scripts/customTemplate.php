d9d1a0d9294b0bd22ad6497c766c89e205e87576
Pete Nerantzinis <17956374+pete-nerantzinis@users.noreply.github.com>
2023-03-04 19:43:31 -0500

    Update customTemplate.php

--- libs/dev_only_scripts/customTemplate.php
+++ libs/dev_only_scripts/customTemplate.php
@@ -6,7 +6,7 @@ checkTemplate($dir);
 
 function checkTemplate($folder) {
     if (is_dir($folder . '/.svn')) {
-        if (is_dir($folder . '/templates/custom_override') || is_dir($folder . '/templates/reports' || is_dir($folder . '/scripts/events'))) {
+        if (is_dir($folder . '/templates/custom_override') || is_dir($folder . '/templates/reports')) {
             $events = scandir($folder . '/templates/custom_override');
             $events2 = scandir($folder . '/templates/reports');
 
@@ -24,16 +24,6 @@ function checkTemplate($folder) {
                     }
                 }
             }
-            foreach ($events3 as $event) {
-                if ($event != '.' && $event != '..' 
-                    && $event != 'CustomEvent_LeafSecure_Certified.php'
-                    && $event != 'CustomEvent_LeafSecure_DeveloperConsole.php'
-                    && $event != 'TEMPLATE_CustomEvent_YOUR_ID.php'
-                    && strpos($event, 'CustomEvent') !== false
-                ) {
-                    cleanEvent($folder . '/scripts/events' . '/' . $event, str_replace('.php', '', $event));
-                }
-            }
         }
     } else {
         $items = scandir($folder);
@@ -46,40 +36,11 @@ function checkTemplate($folder) {
     }
 }
 
+
 function cleanFile($fileName) {
     $file_contents = file_get_contents($fileName);
     $file_contents = str_replace("../libs/dynicons", "dynicons", $file_contents);
     $file_contents = str_replace("../libs/qrcode", '{$abs_portal_path}/qrcode', $file_contents);
-    $file_contents = str_replace("https://leaf.va.gov/", "/", $file_contents);
-    file_put_contents($fileName, $file_contents);
-}
-
-function cleanEvent($fileName, $className) {
-    $myline = getLine($fileName, 'class ' . $className);
-    $commentEnd = getLine($fileName, '*/') + 1;
-    $contents = file($fileName);
-    $keep2 = array_slice($contents, $myline);
-
-    $a = array_splice($contents, 0, $commentEnd);
-
-    $keep = array_merge($a, $keep2);
-
-    file_put_contents($fileName, $keep);
-
-    $file_contents = file_get_contents($fileName);
-    $file_contents = str_replace("class " . $className, "\nnamespace Portal;\n\nclass " . $className, $file_contents);
-    $file_contents = str_replace("include_once '../form.php';", "", $file_contents);
-    $file_contents = str_replace("include_once '../FormWorkflow.php';", "", $file_contents);
-    $file_contents = str_replace("new DB", "new \Leaf\Db", $file_contents);
-    $file_contents = str_replace("DIRECTORY_HOST", "\DIRECTORY_HOST", $file_contents);
-    $file_contents = str_replace("DIRECTORY_USER", "\DIRECTORY_USER", $file_contents);
-    $file_contents = str_replace("DIRECTORY_PASS", "\DIRECTORY_PASS", $file_contents);
-    $file_contents = str_replace("DIRECTORY_DB", "\DIRECTORY_DB", $file_contents);
-    $file_contents = str_replace("new COM", "new \COM", $file_contents);
-    $file_contents = str_replace("include_once '../../libs/smarty/Smarty.class.php';", "", $file_contents);
-    $file_contents = str_replace("Smarty", "\Smarty", $file_contents);
-    $file_contents = str_replace('include_once __DIR__ . "/../../db_config.php";', "", $file_contents);
-    $file_contents = str_replace("include_once 'form.php';", "", $file_contents);
     $file_contents = str_replace("https://leaf.va.gov/", "/", $file_contents);
     file_put_contents($fileName, $file_contents);
 }
