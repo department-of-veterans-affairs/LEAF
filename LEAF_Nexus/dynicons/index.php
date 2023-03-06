6e497de7dccfc9b409fd349b02a92fe3ca1b60c9
Jamie P Holcomb <jamie.holcomb@adaptivestack.com>
2023-03-06 10:02:12 -0500

    qrode and dynicons again

--- LEAF_Nexus/dynicons/index.php
+++ LEAF_Nexus/dynicons/index.php
@@ -22,8 +22,9 @@ ini_set('display_errors', 0);
 
 */
 
+require_once '../globals.php';
 //require_once LIB_PATH . '/loaders/Leaf_autoloader.php';
-include_once LIB_PATH .  '/php-commons/Dynicon.php';
+include_once LIB_PATH .  'php-commons/Dynicon.php';
 include_once LIB_PATH .  '/php-commons/XSSHelpers.php';
 
 $image = new \Leaf\Dynicon(\Leaf\XSSHelpers::scrubFilename($_GET['img']), $_GET['w']);
