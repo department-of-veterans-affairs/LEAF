e57693c94b2c2cbb08dd5104e6deca13b63fa910
Pete Nerantzinis <17956374+pete-nerantzinis@users.noreply.github.com>
2023-03-21 13:49:07 -0400

    Hotfix DB Update

--- docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2023012600-2023022300.sql
+++ docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2023012600-2023022300.sql
@@ -5,7 +5,7 @@ ALTER TABLE signatures MODIFY recordID mediumint(8) UNSIGNED NOT NULL;
 ALTER TABLE signatures MODIFY stepID smallint(6) UNSIGNED NOT NULL;
 ALTER TABLE signatures MODIFY dependencyID smallint(6) UNSIGNED NOT NULL;
 ALTER TABLE tags MODIFY recordID mediumint(8) UNSIGNED NOT NULL;
-ALTER TABLE groups MODIFY parentGroupID mediumint(9) UNSIGNED NOT NULL;
+ALTER TABLE `groups` MODIFY parentGroupID mediumint(9) UNSIGNED NOT NULL;
 
 UPDATE `settings` SET `data` = '2023022300' WHERE `settings`.`setting` = 'dbversion';
 
@@ -20,7 +20,7 @@ ALTER TABLE signatures MODIFY recordID smallint(5) UNSIGNED NOT NULL;
 ALTER TABLE signatures MODIFY stepID smallint(5) UNSIGNED NOT NULL;
 ALTER TABLE signatures MODIFY dependencyID smallint(5) UNSIGNED NOT NULL;
 ALTER TABLE tags MODIFY recordID int(11) UNSIGNED NOT NULL;
-ALTER TABLE groups MODIFY parentGroupID tinyint(4) UNSIGNED NOT NULL;
+ALTER TABLE `groups` MODIFY parentGroupID tinyint(4) UNSIGNED NOT NULL;
 
 UPDATE `settings` SET `data` = '2023012400' WHERE `settings`.`setting` = 'dbversion';
 
