739f6c4147425e3dc862f1f83f3920a81f8dd5d1
Michael E. Shaffer <pelentan@gmail.com>
2023-03-22 15:56:31 -0400

    working on secrets

--- docker/scripts/startup.sh
+++ docker/scripts/startup.sh
@@ -2,7 +2,13 @@
 export DOLLAR="$"
 envsubst < /etc/nginx/leaf_nginx.template > /etc/nginx/sites-enabled/default
 
-./ss.sh
+export KRB_USER=$(cat /run/secrets/krb_user)
+export KRB_PASSWORD=$(cat /run/secrets/krb_pass)
+export CDW_PASSWORD=$(cat /run/secrets/cdw_pass)
+export CDW_USER=$(cat /run/secrets/cdw_user)
+export CDW_HOST=$(cat /run/secrets/cdw_host)
+export DATABASE_PASSWORD=$(cat /run/secrets/db_password)
+export CIPHER_KEY=$(cat /run/secrets/cipher_key)
 
 nginx
 
