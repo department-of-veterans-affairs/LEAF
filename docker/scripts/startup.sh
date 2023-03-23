cd94a488caf4555af57d9cc4164adc4badc280ea
Michael E. Shaffer <pelentan@gmail.com>
2023-03-23 10:01:35 -0400

    adding start-up service

--- docker/scripts/startup.sh
+++ docker/scripts/startup.sh
@@ -2,14 +2,6 @@
 export DOLLAR="$"
 envsubst < /etc/nginx/leaf_nginx.template > /etc/nginx/sites-enabled/default
 
-export KRB_USER=$(cat /run/secrets/krb_user)
-export KRB_PASSWORD=$(cat /run/secrets/krb_pass)
-export CDW_PASSWORD=$(cat /run/secrets/cdw_pass)
-export CDW_USER=$(cat /run/secrets/cdw_user)
-export CDW_HOST=$(cat /run/secrets/cdw_host)
-export DATABASE_PASSWORD=$(cat /run/secrets/db_password)
-export CIPHER_KEY=$(cat /run/secrets/cipher_key)
-
 nginx
 
 php-fpm --nodaemonize
