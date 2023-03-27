a21e7a86dab372fd9f5cbf70b94d5e3c74de8482
Michael E. Shaffer <pelentan@gmail.com>
2023-03-27 15:09:45 -0400

    trying to get it running

--- docker/scripts/startup.sh
+++ docker/scripts/startup.sh
@@ -2,6 +2,18 @@
 export DOLLAR="$"
 envsubst < /etc/nginx/leaf_nginx.template > /etc/nginx/sites-enabled/default
 
+set -a
+export KRB_USER=$(cat /run/secrets/krb_user)
+export KRB_PASSWORD=$(cat /run/secrets/krb_pass)
+export CDW_PASSWORD=$(cat /run/secrets/cdw_pass)
+export CDW_USER=$(cat /run/secrets/cdw_user)
+export CDW_HOST=$(cat /run/secrets/cdw_host)
+export DATABASE_PASSWORD=$(cat /run/secrets/db_password)
+export DATABASE_HOST=$(cat /run/secrets/db_host)
+export DATABAASE_DB_ADMIN=$(cat /run/secrets/db_admin)
+export CIPHER_KEY=$(cat /run/secrets/cipher_key)
+set +a
+
 nginx
 
 php-fpm --nodaemonize
