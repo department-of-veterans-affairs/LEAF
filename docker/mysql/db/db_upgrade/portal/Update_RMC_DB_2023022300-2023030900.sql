1e6ce69bb37b6da30fea6dcdbdc54f17c69ebc15
Juan Rodriguez <info@jrodcode.com>
2023-04-04 12:33:09 -0700

    Create Update_RMC_DB_2023022300-2023030900.sql

---
+++ docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2023022300-2023030900.sql
@@ -0,0 +1,29 @@
+START TRANSACTION;
+
+CREATE TABLE `template_history_files` (
+  `file_id` int(11) NOT NULL AUTO_INCREMENT,
+  `file_parent_name` text,
+  `file_name` text,
+  `file_path` text,
+  `file_size` mediumint(9) DEFAULT NULL,
+  `file_modify_by` text,
+  `file_created` text,
+  PRIMARY KEY (`file_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+UPDATE `settings` SET `data` = '2023040600' WHERE `settings`.`setting` = 'dbversion';
+
+COMMIT;
+
+
+/**** Revert DB *****
+
+START TRANSACTION;
+
+DROP TABLE IF EXISTS `template_history_files`;
+
+UPDATE `settings` SET `data` = '2023030900' WHERE `settings`.`setting` = 'dbversion';
+
+COMMIT;
+
+*/
