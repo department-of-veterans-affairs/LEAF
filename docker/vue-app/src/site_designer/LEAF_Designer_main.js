a4a833b033449701d7e9650ee8cdd9b37af2c79b
Carrie Hanscom <CHanscom7@gmail.com>
2023-06-26 15:03:23 -0400

    LEAF 3782 rm deprecated setting in main files, fix loader img, fix live-side cond check

--- docker/vue-app/src/site_designer/LEAF_Designer_main.js
+++ docker/vue-app/src/site_designer/LEAF_Designer_main.js
@@ -4,8 +4,5 @@ import router from "./router";
 
 const app = createApp(LEAF_Designer_App);
 
-/* This opt-in config setting is used to allow computed injections to be used without
-the value property.  per Vue dev, will not be needed after next minor update */
-app.config.unwrapInjectedRef = true;
 app.use(router);
 app.mount('#site-designer-app');
