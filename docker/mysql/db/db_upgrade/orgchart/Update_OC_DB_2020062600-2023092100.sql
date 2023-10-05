907002ed112402972b87247db100f741e74a860e
Pete Nerantzinis <17956374+pete-nerantzinis@users.noreply.github.com>
2023-10-05 12:07:29 -0400

    Update DB

---
+++ docker/mysql/db/db_upgrade/orgchart/Update_OC_DB_2020062600-2023092100.sql
@@ -0,0 +1,18 @@
+START TRANSACTION;
+
+ALTER TABLE `relation_employee_backup` ADD INDEX `backupEmpUID` (`backupEmpUID`);
+
+UPDATE `settings` SET `data` = '2023092100' WHERE `settings`.`setting` = 'dbversion';
+
+COMMIT;
+
+
+/**** Revert DB *****
+START TRANSACTION;
+
+ALTER TABLE `relation_employee_backup` DROP INDEX `backupEmpUID`;
+
+UPDATE `settings` SET `data` = '2020062600' WHERE `settings`.`setting` = 'dbversion';
+
+COMMIT;
+*/
