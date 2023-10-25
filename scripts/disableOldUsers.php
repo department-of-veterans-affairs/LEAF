6843ab68e5dd5487610db1d7d3d950543261a037
Shane O <shane.ottinger@adaptivestack.com>
2023-10-25 14:54:14 -0500

    LEAF-4019 - setup script to disable old users, look into ways to prevent users that should not be deleted from being deleted is going to be the next step.

---
+++ scripts/disableOldUsers.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once('timebracketcmd.php');
+echo date('Y-m-d g:i:s a') . "\r\n";
+
+try {
+
+    // in the future stuff like this will be spun off through a different program.
+    // Until then to keep thinks straight forward this could be done in individual files to be called.
+    $test = new TimeBracketCmd('disableOldUsers.php');
+
+    $test->setRunAtExactTime('1 am');
+    $test->run();
+    echo 'end';
+    echo date('Y-m-d g:i:s a') . "\r\n";
+
+} catch (Exception $e) {
+    echo sprintf("Message: %s \r\nFile: %s \r\nLine: %s \r\nTrace: %s\r\n", $e->getMessage(), $e->getFile(), $e->getLine(), $e->getTraceAsString());
+}
