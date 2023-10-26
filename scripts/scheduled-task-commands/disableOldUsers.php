2a9029f03c12c17b97322b2b6bf16785e387ef79
Shane O <shane.ottinger@adaptivestack.com>
2023-10-26 09:15:53 -0500

    LEAF-4019 - reduce this down to one query, reduce it down to 1 week.

--- scripts/scheduled-task-commands/disableOldUsers.php
+++ scripts/scheduled-task-commands/disableOldUsers.php
@@ -5,25 +5,9 @@ require_once LIB_PATH . '/php-commons/Db.php';
 // national_orgchart for stage/live leaf_user for dev
 $db = new Leaf\Db(DIRECTORY_HOST, DIRECTORY_USER, DIRECTORY_PASS, 'national_orgchart');
 
-$vars = [
-    ':lastUpdated' => strtotime('-2 weeks')
-    //':lastUpdated' => 1623606681
+$employeeVars = [
+    ':lastUpdated' => strtotime('-1 week'),
+    ':deleted' => time()
 ];
-
-$sql = "SELECT empUID, userName, lastUpdated FROM `employee` Where `lastUpdated` > 0 and `lastUpdated` < :lastUpdated and `deleted` = 0";
-
-$employees = $db->prepared_query($sql,$vars);
-
-// set this all to the same to help with when things were deleted.
-$deletedTime = time();
-foreach( $employees as $employee ){
-
-    // we could use a bit of the logic from checkLastLogin.php to see if the user has been logged in recently. 
-
-    $employeeVars = [
-        ':empUID' => $employee['empUID'],
-        ':deleted' => $deletedTime
-    ];
-    $employeeSql = 'UPDATE `employee` SET `deleted`=:deleted WHERE empUID=:empUID';
-    $db->prepared_query($employeeSql, $employeeVars);
-}
+$employeeSql = 'UPDATE `employee` SET `deleted`=:deleted WHERE `lastUpdated` > 0 and `lastUpdated` < :lastUpdated and `deleted` = 0';
+$db->prepared_query($employeeSql, $employeeVars);
