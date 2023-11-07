c3110a849222d046ba4e9d191ba8a6174f574bfa
mgaoVA <16783916+mgaoVA@users.noreply.github.com>
2023-11-07 09:50:07 -0500

    initial integration proof of concept

---
+++ x-test/API-tests/formQuery_test.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"strings"
+	"testing"
+)
+
+func getFormQuery(url string) (FormQueryResponse, error) {
+	url = strings.Replace(url, " ", "%20", -1)
+	res, _ := client.Get(url)
+	b, _ := io.ReadAll(res.Body)
+
+	var m FormQueryResponse
+	err := json.Unmarshal(b, &m)
+
+	return m, err
+}
+
+func TestFormQuery_HomepageQuery(t *testing.T) {
+	res, _ := getFormQuery(rootURL + `api/form/query?q={"terms":[{"id":"title","operator":"LIKE","match":"***","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service","status","categoryName"],"sort":{"column":"date","direction":"DESC"},"limit":50}`)
+
+	// get first key
+	var key int
+	for k, _ := range res {
+		key = k
+		break
+	}
+
+	got := res[key].RecordID
+	want := key
+
+	if got != want {
+		t.Errorf("RecordID = %v, want = %v", got, want)
+	}
+}
+
+func TestFormQuery_NonadminQuery(t *testing.T) {
+	res, _ := getFormQuery(rootURL + `api/form/query?q={"terms":[{"id":"stepID","operator":"!=","match":"resolved","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service"],"sort":{},"limit":1000,"limitOffset":0}&x-filterData=recordID,title&masquerade=nonAdmin`)
+
+	if _, exists := res[958]; exists {
+		t.Errorf("Record 958 should not be readable")
+	}
+
+	if _, exists := res[530]; !exists {
+		t.Errorf("Record 530 should be readable")
+	}
+}
+
+func TestFormQuery_NonadminQueryActionable(t *testing.T) {
+	res, _ := getFormQuery(rootURL + `api/form/query?q={"terms":[{"id":"stepID","operator":"=","match":"actionable","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service"],"sort":{},"limit":1000,"limitOffset":0}&x-filterData=recordID,title&masquerade=nonAdmin`)
+
+	if _, exists := res[503]; !exists {
+		t.Errorf("Record 503 should be actionable because tester is backup of person designated")
+	}
+
+	if _, exists := res[504]; !exists {
+		t.Errorf("Record 504 should be actionable because tester is backup of initiator")
+	}
+
+	if _, exists := res[505]; exists {
+		t.Errorf("Record 505 should not be actionable because tester is not the requestor")
+	}
+
+	if _, exists := res[500]; !exists {
+		t.Errorf("Record 500 should be actionable because tester is the designated reviewer")
+	}
+
+	if _, exists := res[531]; !exists {
+		t.Errorf("Record 531 should be actionable because tester is a member of the designated group")
+	}
+
+	if _, exists := res[532]; exists {
+		t.Errorf("Record 532 should not be actionable because tester is a member of the designated group")
+	}
+}
+
+func TestFormQuery_FulltextSearch_ApplePearOrange(t *testing.T) {
+	res, _ := getFormQuery(rootURL + `api/form/query?q={"terms":[{"id":"data","indicatorID":"3","operator":"MATCH","match":"apple pear orange","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service","status","categoryName"],"sort":{"column":"date","direction":"DESC"},"limit":50}`)
+
+	if _, exists := res[499]; !exists {
+		t.Errorf(`Record 499 should exist because a data field contains either apple, pear, or orange`)
+	}
+
+	if _, exists := res[498]; !exists {
+		t.Errorf(`Record 498 should exist because a data field contains either apple, pear, or orange`)
+	}
+}
+
+func TestFormQuery_FulltextSearch_ApplePear_RequireOrange(t *testing.T) {
+	res, _ := getFormQuery(rootURL + `api/form/query?q={"terms":[{"id":"data","indicatorID":"3","operator":"MATCH","match":"apple pear %2Borange","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service","status","categoryName"],"sort":{"column":"date","direction":"DESC"},"limit":50}`)
+
+	if _, exists := res[499]; !exists {
+		t.Errorf(`Record 499 should exist because a data field contains the word "orange"`)
+	}
+
+	if _, exists := res[498]; exists {
+		t.Errorf(`Record 498 should not exist because data fields do not contain the word "orange"`)
+	}
+}
+
+func TestFormQuery_FulltextSearch_ApplePearNoOrange(t *testing.T) {
+	res, _ := getFormQuery(rootURL + `api/form/query?q={"terms":[{"id":"data","indicatorID":"3","operator":"MATCH","match":"apple pear %2Dorange","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service","status","categoryName"],"sort":{"column":"date","direction":"DESC"},"limit":50}`)
+
+	if _, exists := res[499]; exists {
+		t.Errorf(`Record 499 should not exist because data fields contain the word "orange". want = no orange`)
+	}
+
+	if _, exists := res[498]; !exists {
+		t.Errorf(`Record 498 should exist because data fields do not contain the word "orange"`)
+	}
+}
---
+++ x-test/API-tests/form_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"io"
+	"net/url"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestForm_Version(t *testing.T) {
+	got, _ := httpGet(rootURL + "api/form/version")
+	want := `"1"`
+
+	if !cmp.Equal(got, want) {
+		t.Errorf("form version = %v, want = %v", got, want)
+	}
+}
+
+func TestForm_AdminCanEditData(t *testing.T) {
+	postData := url.Values{}
+	postData.Set("CSRFToken", csrfToken)
+	postData.Set("3", "12345")
+
+	res, _ := client.PostForm(rootURL+`api/form/505`, postData)
+	bodyBytes, _ := io.ReadAll(res.Body)
+	got := string(bodyBytes)
+	want := `"1"`
+
+	if !cmp.Equal(got, want) {
+		t.Errorf("Admin got = %v, want = %v", got, want)
+	}
+}
+
+func TestForm_NonadminCannotEditData(t *testing.T) {
+	postData := url.Values{}
+	postData.Set("CSRFToken", csrfToken)
+	postData.Set("3", "12345")
+
+	res, _ := client.PostForm(rootURL+`api/form/505?masquerade=nonAdmin`, postData)
+	bodyBytes, _ := io.ReadAll(res.Body)
+	got := string(bodyBytes)
+	want := `"0"`
+
+	if !cmp.Equal(got, want) {
+		t.Errorf("Non-admin got = %v, want = %v", got, want)
+	}
+}
+
+func TestForm_WorkflowIndicatorAssigned(t *testing.T) {
+	got, res := httpGet(rootURL + "api/form/508/workflow/indicator/assigned")
+
+	if !cmp.Equal(res.StatusCode, 200) {
+		t.Errorf("./api/form/508/workflow/indicator/assigned Status Code = %v, want = %v", res.StatusCode, 200)
+	}
+
+	want := `[]`
+	if !cmp.Equal(got, want) {
+		t.Errorf("./api/form/508/workflow/indicator/assigned = %v, want = %v", got, want)
+	}
+}
