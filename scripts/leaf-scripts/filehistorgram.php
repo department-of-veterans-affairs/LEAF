499606abe6a63fa88e825edda5103bec5c8102a9
Shane O <shane.ottinger@adaptivestack.com>
2023-11-15 10:17:35 -0600

    LEAF-2478 - Get the filehistogram file in place, also bring over showdbvers which hadnt been dropped in here yet.

---
+++ scripts/leaf-scripts/filehistorgram.php
@@ -0,0 +1,54 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require_once 'globals.php';
+require_once LIB_PATH . '/php-commons/Db.php';
+
+$startTime = microtime(true);
+
+$db = new Leaf\Db(DIRECTORY_HOST, DIRECTORY_USER, DIRECTORY_PASS, 'national_leaf_launchpad');
+
+$portals = $db->query("SELECT * FROM `sites` WHERE `site_type` = 'portal'");
+$directory = '/var/www/html';
+$totalfilecount = 0;
+$totalfilesize = 0;
+$fp = fopen('filehistogram.csv', 'w');
+fputcsv($fp, ['path', 'max (MB)', 'min (MB)', 'average (MB)', 'count']);
+foreach ($portals as $portal) {
+
+    $filesizes = ['path' => $portal['site_path'], 'max' => 0, 'min' => 999999999, 'average' => 0, 'count' => 0];
+    $header = array_keys($filesizes);
+
+    $glob_array = glob($directory . $portal['site_path'] . '/files/*.*');
+    $filecount = count($glob_array);
+    $filzesize['count'] = $filecount;
+
+    foreach ($glob_array as $glob) {
+        $filesize = filesize($glob);
+        if ($filesize > 0)
+            $filesize = $filesize / 1024;
+
+        if ($filesize > $filesizes['max']) {
+            $filesizes['max'] = $filesize;
+        }
+
+        if ($filesize < $filesizes['min']) {
+            $filesizes['min'] = $filesize;
+        }
+
+        $filesizes['average'] += $filesize;
+    }
+
+    if (count($glob_array) > 0) {
+        $totalfilesize += $filesizes['average'];
+        $filesizes['average'] = $filesizes['average'] / $filecount;
+        $totalfilecount += $filecount;
+        fputcsv($fp, $filesizes);
+    }
+}
+fputcsv($fp, ['total', 0, 0, $totalfilesize / $totalfilecount, $totalfilecount]);
+fclose($fp);
+$endTime = microtime(true);
+$timeInMinutes = round(($endTime - $startTime) / 60, 2);
+echo "Destruction took {$timeInMinutes} minutes\r\n";
+echo date('Y-m-d g:i:s a') . "\r\n";
---
+++ scripts/leaf-scripts/showdbvers.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'globals.php';
+require_once LIB_PATH . '/php-commons/Db.php';
+
+$startTime = microtime(true);
+
+$db = new Leaf\Db(DIRECTORY_HOST, DIRECTORY_USER, DIRECTORY_PASS, 'national_leaf_launchpad');
+
+$portals = $db->query("SELECT `portal_database` FROM `sites` WHERE `site_type` = 'portal'");
+
+foreach ($portals as $portal) {
+    // Initialize record destruction counter
+    $count = 0;
+
+    // Switch to portal in list
+    $db->query("USE `{$portal['portal_database']}`");
+
+    $results = $db->query("SELECT `data` from `settings` where setting = 'dbversion'");
+
+    if( (int)$results[0]['data'] < 2023100500){
+        var_dump($portal['portal_database'],$results[0]['data']);
+    }
+}
+
+$endTime = microtime(true);
+$timeInMinutes = round(($endTime - $startTime) / 60, 2);
+echo "Destruction took {$timeInMinutes} minutes\r\n";
+echo date('Y-m-d g:i:s a') . "\r\n";
