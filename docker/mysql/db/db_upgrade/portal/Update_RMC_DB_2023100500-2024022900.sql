e62f3a820e7cd932e1d44babc6baa1d3ab081502
Jamie P Holcomb <jph3377@gmail.com>
2024-02-06 13:40:26 -0500

    Leaf 4204 - added sites table to all portals and added the two new fields.

---
+++ docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2023100500-2024022900.sql
@@ -0,0 +1,27 @@
+START TRANSACTION;
+
+CREATE TABLE `sites` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `site_type` varchar(8) NOT NULL,
+  `site_path` varchar(191) NOT NULL,
+  `site_uploads` varchar(191) DEFAULT NULL,
+  `portal_database` varchar(50) DEFAULT NULL,
+  `orgchart_path` varchar(191) DEFAULT NULL,
+  `orgchart_database` varchar(50) DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `site_path` (`site_path`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ UPDATE `settings` SET `data` = '2024022900' WHERE `settings`.`setting` = 'dbversion';
+
+ COMMIT;
+
+
+ /**** Revert DB *****
+ START TRANSACTION;
+ DROP TABLE IF EXISTS `process_query`;
+ UPDATE `settings` SET `data` = '2023091100' WHERE `settings`.`setting` = 'dbversion';
+ COMMIT;
+ */
