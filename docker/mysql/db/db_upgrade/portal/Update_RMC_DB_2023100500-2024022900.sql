40006b7af15e7d38178f69344163667f8c65ff68
Jamie P Holcomb <jph3377@gmail.com>
2024-02-14 13:38:52 -0500

    Leaf 4204 - site creation update

--- docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2023100500-2024022900.sql
+++ docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2023100500-2024022900.sql
@@ -1,18 +1,19 @@
 START TRANSACTION;
 
 CREATE TABLE `sites` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `site_type` varchar(8) NOT NULL,
-  `site_path` varchar(191) NOT NULL,
-  `site_uploads` varchar(191) DEFAULT NULL,
-  `portal_database` varchar(50) DEFAULT NULL,
-  `orgchart_path` varchar(191) DEFAULT NULL,
-  `orgchart_database` varchar(50) DEFAULT NULL,
+  `site_path` varchar(250) NOT NULL,
+  `site_uploads` varchar(250) DEFAULT NULL,
+  `portal_database` varchar(250) DEFAULT NULL,
+  `orgchart_path` varchar(250) DEFAULT NULL,
+  `orgchart_database` varchar(250) DEFAULT NULL,
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `site_path` (`site_path`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `site_path` (`site_path`),
+  KEY `site_type` (`site_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
  UPDATE `settings` SET `data` = '2024022900' WHERE `settings`.`setting` = 'dbversion';
 
