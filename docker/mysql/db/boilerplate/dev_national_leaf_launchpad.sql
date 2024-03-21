bb098105e981e1ba3bb8f8f4bebf78c0e29fccc0
mgaoVA <16783916+mgaoVA@users.noreply.github.com>
2024-03-21 19:38:20 -0400

    minimal db to work

---
+++ docker/mysql/db/boilerplate/dev_national_leaf_launchpad.sql
@@ -0,0 +1,3 @@
+INSERT INTO `sites` (`id`, `launchpadID`, `site_type`, `site_path`, `site_uploads`, `portal_database`, `orgchart_path`, `orgchart_database`, `decommissionTimestamp`, `updated`, `created`) VALUES
+(1,	0,	'portal',	'/LEAF_Request_Portal',	'/var/www/html/LEAF_Request_Portal/UPLOADS',	'leaf_portal',	'/LEAF_Nexus',	'leaf_users',	0,	'2022-12-21 12:57:16',	'2022-12-21 12:57:16'),
+(2,	0,	'orgchart',	'/LEAF_Nexus',	'/var/www/html/LEAF_Nexus/UPLOADS',	NULL,	'/LEAF_Nexus',	'leaf_users',	0,	'2022-12-21 12:57:46',	'2022-12-21 12:57:46');
--- docker/mysql/dev_bootstrap.sh
+++ docker/mysql/dev_bootstrap.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 mysql -u root -ptester -e "CREATE USER tester@'localhost' IDENTIFIED BY 'tester'"
 mysql -u root -ptester -e "GRANT ALL PRIVILEGES ON * . * TO 'tester'@'localhost';"
 mysql -u root -ptester -e "GRANT ALL PRIVILEGES ON * . * TO 'tester'@'%'; FLUSH PRIVILEGES;"
@@ -7,5 +8,10 @@ mysql -u tester -ptester -e "create database leaf_portal"
 mysql -u tester -ptester -e "create database leaf_users"
 
 mysql -u tester -ptester national_leaf_launchpad < /leaf-boilerplate/resource_database_boilerplate.sql
+mysql -u tester -ptester national_leaf_launchpad < /leaf-boilerplate/dev_national_leaf_launchpad.sql
+
 mysql -u tester -ptester leaf_portal < /leaf-boilerplate/resource_database_boilerplate.sql
+mysql -u tester -ptester leaf_portal < /leaf-boilerplate/dev_leaf_portal.sql
+
 mysql -u tester -ptester leaf_users < /leaf-boilerplate/orgchart_boilerplate_empty.sql
+mysql -u tester -ptester leaf_users < /leaf-boilerplate/dev_leaf_users.sql
