3ca81abbc6606c0cafd5a7bc123fc6d9aacc809a
Casey Herold <caseynate@gmail.com>
2024-04-03 07:06:04 -0600

    keep localhost as url for selenium chrome driver

--- test/Test-Automation/selenium-test-helper/index.php
+++ test/Test-Automation/selenium-test-helper/index.php
@@ -1,5 +1,5 @@
 <?php
-  $result = exec("cd /app && mvn test -Dremote.url=http://192.168.0.16:4445 -Dapp.url=host.docker.internal/LEAF_Request_Portal/admin/ -Dclass.name=test.java.formWorkflow.formWorkflow_Test", $output, $return_var);
+  $result = exec("cd /app && mvn test -Dremote.url=http://localhost:4445 -Dapp.url=host.docker.internal/LEAF_Request_Portal/admin/ -Dclass.name=test.java.formWorkflow.formWorkflow_Test", $output, $return_var);
 
   if ($return_var === 0) {
     // Output the results
