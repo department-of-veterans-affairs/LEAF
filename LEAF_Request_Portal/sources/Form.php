03222c571d7392306ce46718d431bb42aa4e4fb8
Carrie <chanscom7@gmail.com>
2024-05-09 12:32:43 -0400

    LEAF 4344 comment out unused query

--- LEAF_Request_Portal/sources/Form.php
+++ LEAF_Request_Portal/sources/Form.php
@@ -1130,7 +1130,7 @@ class Form
             $this->db->prepared_query('INSERT INTO data_history (recordID, indicatorID, series, data, metadata, timestamp, userID)
                                                    VALUES (:recordID, :indicatorID, :series, :data, :metadata, :timestamp, :userID)', $vars);
         }
-
+        /*  signatures (not yet implemented)
         $vars = array(':recordID' => $recordID,
                       ':indicatorID' => $key,
                       ':series' => $series, );
@@ -1141,7 +1141,7 @@ class Form
 
         if (strpos($res[0]['format'], 'signature') == 0) {
             // $this->writeSignature($recordID);
-        }
+        }*/
         return 1;
     }
 
