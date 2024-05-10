52de23c4ed38eab3c40c0193ec9b9cf865431568
mgaoVA <16783916+mgaoVA@users.noreply.github.com>
2024-05-10 08:20:24 -0400

    update db version

--- docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2024022901-2024050900.sql
+++ docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2024022901-2024050900.sql
@@ -2,7 +2,7 @@ START TRANSACTION;
 
 ALTER TABLE `records` ADD `lastActionTime` int unsigned NULL AFTER `lastStatus`;
 
-UPDATE `settings` SET `data` = '2024022901' WHERE `settings`.`setting` = 'dbversion';
+UPDATE `settings` SET `data` = '2024050900' WHERE `settings`.`setting` = 'dbversion';
 
 COMMIT;
 
