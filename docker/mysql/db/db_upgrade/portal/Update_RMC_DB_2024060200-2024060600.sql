cb94fa5fdd5dfa6642800b330baa43ad7feea35a
Jamie P Holcomb <jph3377@gmail.com>
2024-06-06 09:41:24 -0400

    Leaf 3135 - db update name change

--- docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2024060200-2024060600.sql
+++ docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2024060200-2024060600.sql
@@ -3,7 +3,7 @@ START TRANSACTION;
 INSERT INTO `email_templates` (`emailTemplateID`, `label`, `emailTo`, `emailCc`, `subject`, `body`)
 VALUES ('-7', 'Cancel Notification', 'LEAF_cancel_notification_emailTo.tpl', 'LEAF_cancel_notification_emailCc.tpl', 'LEAF_cancel_notification_subject.tpl', 'LEAF_cancel_notification_body.tpl');
 
-UPDATE `settings` SET `data` = '2024060700' WHERE `settings`.`setting` = 'dbversion';
+UPDATE `settings` SET `data` = '2024060600' WHERE `settings`.`setting` = 'dbversion';
 
 COMMIT;
 
@@ -12,6 +12,6 @@ COMMIT;
  START TRANSACTION;
  DELETE FROM `email_templates` WHERE `emailTemplateID` = -7;
 
- UPDATE `settings` SET `data` = '2024052000' WHERE `settings`.`setting` = 'dbversion';
+ UPDATE `settings` SET `data` = '2024060200' WHERE `settings`.`setting` = 'dbversion';
  COMMIT;
  */
