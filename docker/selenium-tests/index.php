0e828f8e80691c5d0de119b48171e443bb399fef
pelentan <pelentan@gmail.com>
2024-08-01 09:44:35 -0400

    tweaks for structure changes

--- docker/selenium-tests/index.php
+++ docker/selenium-tests/index.php
@@ -2,7 +2,7 @@
   $result = exec("mvn test", $output, $return_var);
   if ($return_var === 0) {
     // Display results page
-    include './reports/AutomationResult.html';
+    include './ExtentReports/AutomationResult.html';
 
   } else {
       echo "Error executing command";
