f2112e8e9ccab254d43e685ba7ec6d8da59d7d32
mgaoVA <16783916+mgaoVA@users.noreply.github.com>
2024-08-08 20:54:41 -0400

    runnable via web UI, and trying out test generator

---
+++ x-test/end2end/main.js
@@ -0,0 +1,48 @@
+// Quick way to trigger tests from a web UI
+const http = require('http');
+const { spawn } = require( 'child_process' );
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/api/v1/test' && req.method === 'GET') {
+        runTests(req, res);
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end("This is the end2end test helper");
+    }
+});
+
+server.listen(8001, () => console.log('Server running on port 8001'));
+
+
+var startedTests = false;
+async function runTests(req, res) {
+    if(startedTests) {
+      res.end('Tests already running');
+    }
+    startedTests = true;
+
+    console.log('running tests...');
+    let cmd = spawn('npx', ['playwright', 'test']);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    cmd.stdout.on('data', data => {
+      console.log(data.toString());
+    });
+
+    cmd.stderr.on('data', data => {
+      console.log(data.toString());
+    });
+
+    cmd.on('exit', () => {
+      fs.readFile('playwright-report/index.html', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        res.end(data);
+      });
+
+      startedTests = false;
+    });
+}
--- x-test/end2end/tests/example.spec.ts
+++ x-test/end2end/tests/example.spec.ts
@@ -6,3 +6,10 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Leaf/);
 });
+
+test('search record ID', async ({ page }) => {
+  await page.goto('https://host.docker.internal/LEAF_Request_Portal/');
+  await page.getByLabel('Enter your search text').click();
+  await page.getByLabel('Enter your search text').fill('500');
+  await expect(page.getByRole('link', { name: '500' })).toBeVisible();
+});
