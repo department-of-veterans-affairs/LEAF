180ee660e29b4b5ffe8d02779edf5305c26fd3c3
Carrie Hanscom <chanscom7@gmail.com>
2024-08-21 08:13:09 -0400

    LEFA 4490 add data_history.userDisplay to db upgrade file

--- docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2024071100-2024082000.sql
+++ docker/mysql/db/db_upgrade/portal/Update_RMC_DB_2024071100-2024082000.sql
@@ -3,6 +3,8 @@ START TRANSACTION;
 ALTER TABLE `records` ADD COLUMN `userMetadata` json DEFAULT NULL;
 ALTER TABLE `notes` ADD COLUMN `userMetadata` json DEFAULT NULL;
 
+ALTER TABLE `data_history` ADD COLUMN `userDisplay` varchar(90) DEFAULT NULL;
+
 UPDATE `settings` SET `data` = '2024082000' WHERE `settings`.`setting` = 'dbversion';
 
 COMMIT;
@@ -13,6 +15,8 @@ START TRANSACTION;
 
 ALTER TABLE `records` DROP COLUMN `userMetadata`;
 ALTER TABLE `notes` DROP COLUMN `userMetadata`;
+
+ALTER TABLE `data_history` DROP COLUMN `userDisplay`;
 
 UPDATE `settings` SET `data` = '2024071100' WHERE `settings`.`setting` = 'dbversion';
 
