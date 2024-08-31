539f640c2d21b2d1fab91d43962dad38c3b9e7a8
mgaoVA <16783916+mgaoVA@users.noreply.github.com>
2024-08-31 12:54:20 -0400

    add test

---
+++ x-test/end2end/tests/lifecycleDeveloperConsoleApproval.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('no javascript errors on supervisor selection page', async ({ page }) => {
+  let errors = new Array<Error>;
+  page.on('pageerror', err => {
+    errors.push(err);
+  });
+
+  await page.goto('https://host.docker.internal/Test_Request_Portal/report.php?a=LEAF_start_leaf_dev_console_request');
+  await page.getByRole('button', { name: 'I understand and accept' }).click();
+  await page.getByRole('button', { name: 'Next Next Question' }).click();
+  expect(errors).toHaveLength(0);
+});
