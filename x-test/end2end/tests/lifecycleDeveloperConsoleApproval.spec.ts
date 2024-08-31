0eedbf9f0c97cda4be7e62281caf18db8d23220e
mgaoVA <16783916+mgaoVA@users.noreply.github.com>
2024-08-31 13:07:00 -0400

    reduce test flakiness

--- x-test/end2end/tests/lifecycleDeveloperConsoleApproval.spec.ts
+++ x-test/end2end/tests/lifecycleDeveloperConsoleApproval.spec.ts
@@ -9,5 +9,9 @@ test('no javascript errors on supervisor selection page', async ({ page }) => {
   await page.goto('https://host.docker.internal/Test_Request_Portal/report.php?a=LEAF_start_leaf_dev_console_request');
   await page.getByRole('button', { name: 'I understand and accept' }).click();
   await page.getByRole('button', { name: 'Next Next Question' }).click();
+
+  // wait for async request to complete
+  await expect(page.locator('label')).toContainText('Supervisor* Required');
+
   expect(errors).toHaveLength(0);
 });
