31627070ab1cbdcf6528923594bdfe26a72674d0
Carrie Hanscom <chanscom7@gmail.com>
2024-10-09 11:32:41 -0400

    LEAF 4552 simplify userID get, user pdo init to catch invalid db

--- scripts/leaf-scripts/update_records_notes_dh_fields.php
+++ scripts/leaf-scripts/update_records_notes_dh_fields.php
@@ -13,7 +13,7 @@ require_once getenv('APP_LIBS_PATH') . '/../Leaf/Db.php';
 $log_file = fopen("batch_update_records_notes_dh_log.txt", "w") or die("unable to open file");
 $time_start = date_create();
 
-$db = new App\Leaf\Db(DIRECTORY_HOST, DIRECTORY_USER, DIRECTORY_PASS, 'national_leaf_launchpad');
+$launch_db = new App\Leaf\Db(DIRECTORY_HOST, DIRECTORY_USER, DIRECTORY_PASS, 'national_leaf_launchpad');
 
 //get records of each portal db.  Break out vdr for data_history updates.
 $q = "SELECT `portal_database` FROM `sites` WHERE `portal_database` IS NOT NULL AND " .
@@ -21,43 +21,43 @@ $q = "SELECT `portal_database` FROM `sites` WHERE `portal_database` IS NOT NULL 
     //"`portal_database` = 'Academy_Demo1' AND" .
     "`site_type`='portal' ORDER BY id";
 
-$portal_records = $db->query($q);
+$portal_records = $launch_db->query($q);
+$launch_db->__destruct();
 
 $total_portals_count = count($portal_records);
 $processed_portals_count = 0;
 $error_count = 0;
 
-//get org info up front from national.
+//get org info for enabled users from national.
 $orgchart_db = 'national_orgchart';
 $orgchart_time_start = date_create();
 $empMap = array();
 
 try {
-    $db->query("USE `{$orgchart_db}`");
+    $nat_db = new App\Leaf\Db(DIRECTORY_HOST, DIRECTORY_USER, DIRECTORY_PASS, $orgchart_db);
 
     $qEmployees = "SELECT `employee`.`empUID`, `userName`, `lastName`, `firstName`, `middleName`, `deleted`, `data` AS `email` FROM `employee`
         JOIN `employee_data` ON `employee`.`empUID`=`employee_data`.`empUID`
-        WHERE `userName` NOT LIKE 'disabled_%' AND `indicatorID`=6";
+        WHERE `deleted` = 0 AND `indicatorID`=6";
 
-    $resEmployees = $db->query($qEmployees) ?? [];
+    $resEmployees = $nat_db->query($qEmployees) ?? [];
     foreach($resEmployees as $emp) {
         $mapkey = strtoupper($emp['userName']);
-
-        $isActive = $emp['deleted'] === 0;
         $empMap[$mapkey] = array(
-            'userDisplay' => $isActive ? $emp['firstName'] . " " . $emp['lastName'] : "",
+            'userDisplay' => $emp['firstName'] . " " . $emp['lastName'],
             'userMetadata' => json_encode(
                 array(
-                    'userName' => $isActive ? $emp['userName'] : '',
-                    'firstName' => $isActive ? $emp['firstName'] : '',
-                    'lastName' => $isActive ? $emp['lastName'] : '',
-                    'middleName' => $isActive ? $emp['middleName'] : '',
-                    'email' => $isActive ? $emp['email'] : '',
+                    'userName' => $emp['userName'],
+                    'firstName' => $emp['firstName'],
+                    'lastName' => $emp['lastName'],
+                    'middleName' => $emp['middleName'],
+                    'email' => $emp['email']
                 )
             ),
         );
     }
     unset($resEmployees);
+    $nat_db->__destruct();
 
     $orgchart_time_end = date_create();
     $orgchart_time_diff = date_diff($orgchart_time_start, $orgchart_time_end);
@@ -86,19 +86,43 @@ $fields_to_update = array(
     "records" => "userMetadata",
     "data_history" => "userDisplay",
 );
+$json_empty = json_encode(
+    array(
+        "userName" => "",
+        "firstName" => "",
+        "lastName" => "",
+        "middleName" => "",
+        "email" => ""
+    )
+);
+$user_not_found_values = array(
+    "userMetadata" => $json_empty,
+    "userDisplay" => "",
+);
 
-function getUniqueIDBatch(&$db, $batchcount = 0, $table_name):array {
-    $limit = 1000;
-    $offset = $limit * $batchcount;
 
-    $SQL = "SELECT DISTINCT `userID` FROM `$table_name` ORDER BY `userID` LIMIT $limit OFFSET $offset";
+
+function getUniqueIDBatch(&$db, $table_name, $field_name):array {
+    $SQL = "SELECT DISTINCT `userID` FROM `$table_name` WHERE `$field_name` IS NULL LIMIT 1000";
     return $db->query($SQL) ?? [];
 }
 
+$pdo_options = [
+    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+];
 foreach($portal_records as $rec) {
     $portal_db = $rec['portal_database'];
     try {
-        $db->query("USE `{$portal_db}`");
+        //workaround for potential invalid dbs in sites table since the db class will not 
+        //handle them in a way compatible with this script
+        $pdb = new \PDO(
+            "mysql:host=". DIRECTORY_HOST . ";dbname=" . $portal_db . ";charset=UTF8",
+            DIRECTORY_USER,
+            DIRECTORY_PASS,
+            $pdo_options
+        );
+
+        $pdb = new App\Leaf\Db(DIRECTORY_HOST, DIRECTORY_USER, DIRECTORY_PASS, $portal_db);
 
         $batch_tracking = array(
             "notes" => 0,
@@ -120,7 +144,7 @@ foreach($portal_records as $rec) {
             );
 
             $batchcount = 0;
-            while(count($resUniqueIDsBatch = getUniqueIDBatch($db, $batchcount, $table_name)) > 0) {
+            while(count($resUniqueIDsBatch = getUniqueIDBatch($pdb, $table_name, $field_name)) > 0) {
                 $batchcount += 1;
                 $numIDs = count($resUniqueIDsBatch);
 
@@ -129,19 +153,19 @@ foreach($portal_records as $rec) {
                 $metaVars = array();
 
                 foreach ($resUniqueIDsBatch as $idx => $userRec) {
+                    //use mapped info if present, otherwise use empty values.
                     $userInfo = $empMap[strtoupper($userRec['userID'])] ?? null;
-                    //If they are not in the orgchart map don't do anything.
-                    if(isset($userInfo) && isset($userInfo[$field_name])) {
-                        $metaVars[":user_" . $idx] = $userRec['userID'];
-                        $metaVars[":meta_" . $idx] = $userInfo[$field_name];
-                        $sqlUpdateMetadata .= " WHEN :user_" . $idx . " THEN :meta_" . $idx;
-                    }
+                    $metaVars[":user_" . $idx] = $userRec['userID'];
+                    $metaVars[":meta_" . $idx] = isset($userInfo) ?
+                        $userInfo[$field_name] : $user_not_found_values[$field_name];
+
+                    $sqlUpdateMetadata .= " WHEN :user_" . $idx . " THEN :meta_" . $idx;
                 }
                 $sqlUpdateMetadata .= " END";
                 $sqlUpdateMetadata .= " WHERE `$field_name` IS NULL";
 
                 try {
-                    $db->prepared_query($sqlUpdateMetadata, $metaVars);
+                    $pdb->prepared_query($sqlUpdateMetadata, $metaVars);
                     $batch_tracking[$table_name] += 1;
 
                     fwrite(
@@ -156,7 +180,7 @@ foreach($portal_records as $rec) {
                     );
                     $error_count += 1;
                 }
-            } // while remaining unique ids
+            } // while remaining un-updated unique ids
             
             $table_time_end = date_create();
             $table_time_diff = date_diff($table_time_start, $table_time_end);
@@ -170,6 +194,8 @@ foreach($portal_records as $rec) {
 
         $portal_time_end = date_create();
         $portal_time_diff = date_diff($portal_time_start, $portal_time_end);
+        $pdb->__destruct();
+        
         fwrite(
             $log_file,
             "\r\nPortal update took: " . $portal_time_diff->format('%H hr, %i min, %S sec, %f mcr') . "\r\n"
@@ -203,5 +229,5 @@ fwrite(
     "\r\n-----------------------\r\nProcess took: " . $time_diff->format('%H hr, %i min, %S sec, %f mcr') . "\r\n".
     "total portals: " . $total_portals_count . ", portals processed: " . $processed_portals_count . ", error count: " . $error_count . "\r\n"
 );
-$db->__destruct();
+
 fclose($log_file);
