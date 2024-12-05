2348273d2370fcaf7cc16eeff289f34d5c24a138
shane <ottersdangerden@gmail.com>
2024-12-05 09:06:18 -0600

    LEAF-4487 - working on updating the header value to make it more clear where a process ran on. This will help with troubleshooting issues to give clues to where something had run on.

--- LEAF_Request_Portal/sources/Form.php
+++ LEAF_Request_Portal/sources/Form.php
@@ -3084,7 +3084,7 @@ class Form
 
         $externalProcessQuery = $this->isLargeQuery($query);
         // is this a processes worthy of the external process and that we are on not wanting to actually run this query here.
-        if ($externalProcessQuery === true && !empty($_SERVER['LEAF_Large_Queries']) && $_SERVER['LEAF_Large_Queries'] == 'pass_onto_large_query_server') {
+        if ($externalProcessQuery === true && !empty($_SERVER['LEAF_Large_Queries']) && $_SERVER['LEAF_Large_Queries'] == 'process_ran_on_api_server') {
             ob_end_clean();
             http_response_code(306);
             exit();
