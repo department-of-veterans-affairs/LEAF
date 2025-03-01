94c73ae885d9d9eab17ac6c590bcd615e36303b1
mgaoVA <16783916+mgaoVA@users.noreply.github.com>
2025-02-28 21:46:09 -0500

    parse links by segments

--- LEAF_Request_Portal/js/portalHelpers.js
+++ LEAF_Request_Portal/js/portalHelpers.js
@@ -9,29 +9,32 @@ function enableUserContentLinks(element) {
             "dvagov.sharepoint.com": 1,
             "apps.gov.powerapps.us": 1
         }
-        //links must have https, they could have tags. only grab the url content here and filter based on allowlist
-        let matchLinks = htmlContent.match(/(?<=https:\/\/).*?(?=\s|$|"|'|&gt;|<)/gi);
-        matchLinks = Array.from(new Set(matchLinks));
+        // links must have https, they could have tags. only grab the url content here and filter based on allowlist
+        // exclude links within tags
+        let matchLinks = htmlContent.match(/\b(?<=(?<!"|'|>)https:\/\/).*?(?=\s|$|"|'|&gt;|<)\b/gi);
+        if(matchLinks == null) {
+            return;
+        }
         matchLinks = matchLinks.filter(url => {
             const baseurl = (url.split("/")[0] || "").toLowerCase();
             return baseurl.endsWith(".gov") || allowlist[baseurl] === 1;
         });
 
+        let i = 0;
+        let output = '';
         matchLinks.forEach(match => {
             const linkText = match.length <= 50 ? match : match.slice(0,50) + '...';
             const oldText = `https://${match}`;
             const newText =   `<a href="https://${match}" target="_blank">https://${linkText}</a>`;
-            //initial replacement
-            htmlContent = htmlContent.replace(oldText, newText);
-
-            //if user tried to add anchor tags this will replace them. link text was will be replaced by the new link text.
-            const textEscaped = newText.replaceAll(".", "\\.").replaceAll("?", "\\?");
-            const regStr = `(&lt;|<)a\\s+href=['"]${textEscaped}['"](>|&gt;)(.*)?(&lt;|<)/a(>|&gt;)`;
-            const tagReg = new RegExp(regStr, "gi");
-            if(tagReg.test(htmlContent)) {
-                htmlContent = htmlContent.replace(tagReg, newText);
-            }
+            
+            // parse htmlContent by segments, each containing a matching link
+            let end = htmlContent.indexOf(oldText, i);
+            output += htmlContent.substring(i, end + oldText.length).replace(oldText, newText);
+            i = end + oldText.length;
         });
-        element.innerHTML = htmlContent;
+        // don't forget the last segment
+        output += htmlContent.substring(i);
+
+        element.innerHTML = output;
     }
 }
