ade19a6384a6e042e4b130219a8cdf71ce45fca3
mgaoVA <16783916+mgaoVA@users.noreply.github.com>
2025-05-10 20:17:57 -0400

    fix type

--- pkg/form/query/response.go
+++ pkg/form/query/response.go
@@ -53,6 +53,7 @@ type ActionHistory struct {
 
 // Data represents the dynamic data fields in the record
 // The map's key is "id" + the indicatorID of the field
+// TODO: Migrate the key format from "id###"" to "###"
 type Data map[string]string
 
 type OrgchartEmployee struct {
