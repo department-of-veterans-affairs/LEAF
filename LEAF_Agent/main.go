af912b388f5e3c90a93961292eb9072416e5adb3
mgaoVA <16783916+mgaoVA@users.noreply.github.com>
2025-05-10 20:17:48 -0400

    tasks and instructions

--- LEAF_Agent/main.go
+++ LEAF_Agent/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
+	"net/url"
 	"os"
+	"strconv"
 	"time"
+
+	"github.com/department-of-veterans-affairs/LEAF/pkg/form/query"
 )
 
 var client *http.Client
@@ -46,4 +52,67 @@ func main() {
 
 		time.Sleep(10 * time.Second)
 	}
+}
+
+func FindTasks() ([]Task, error) {
+	res, err := HttpGet(`https://` + HTTP_HOST + `/platform/agent/api/form/query/?q={"terms":[{"id":"stepID","operator":"=","match":"2","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":[],"sort":{},"getData":["4","2","5","3","8"]}&x-filterData=`)
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var r query.Response
+	json.Unmarshal(b, &r)
+
+	var tasks []Task
+
+	for i, v := range r {
+		var instructions []Instruction
+		json.Unmarshal([]byte(v.S1["id4"]), &instructions)
+		timeStamp, _ := strconv.ParseInt(v.S1["id8"], 10, 64)
+		log.Println(timeStamp)
+		lastRun := time.Unix(timeStamp, 0)
+
+		task := Task{
+			TaskID:       i,
+			SiteURL:      v.S1["id2"],
+			StepID:       v.S1["id5"],
+			LastRun:      lastRun,
+			Instructions: instructions,
+		}
+		tasks = append(tasks, task)
+	}
+
+	return tasks, nil
+}
+
+func TakeAction(siteURL string, recID int, stpID int, actionType string, comment string) {
+	recordID := strconv.Itoa(recID)
+	stepID := strconv.Itoa(stpID)
+
+	values := url.Values{}
+	values.Add("dependencyID", "-4")
+	values.Add("stepID", stepID)
+	values.Add("actionType", actionType)
+	values.Add("comment", comment)
+
+	HttpPost(siteURL+"formWorkflow/"+recordID+"/apply'", values)
+}
+
+// UpdateRecord updates a record with the provided data.
+// data is a map where the keys are field IDs (indicatorID) and the values are written into the record matching recID
+func UpdateRecord(siteURL string, recID int, data map[int]string) {
+	recordID := strconv.Itoa(recID)
+
+	values := url.Values{}
+
+	for k, v := range data {
+		values.Add(strconv.Itoa(k), v)
+	}
+
+	HttpPost(siteURL+"form/"+recordID, values)
 }
