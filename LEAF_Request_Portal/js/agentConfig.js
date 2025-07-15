c3223c42753fb8663dc34c4aa22e68e50cec81ae
mgaoVA <16783916+mgaoVA@users.noreply.github.com>
2025-07-15 17:32:33 -0400

    work in progress

--- LEAF_Request_Portal/js/agentConfig.js
+++ LEAF_Request_Portal/js/agentConfig.js
@@ -1,3 +1,4 @@
+// LeafAgentConfig parses and renders the UI for an agent's task
 var LeafAgentConfig = function (containerID) {
     let prefixID = "LeafAgentConfig" + Math.floor(Math.random() * 1000) + "_";
     let config = {};
