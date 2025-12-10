9a82c2c5e74c2ecd6f4b53fb37496a395ea57a7d
Shane O. <94810508+shaneodd@users.noreply.github.com>
2025-12-10 06:57:34 -0600

    LEAF-5150 - addjust the queries, it appears the unsigned and auto_increment are required even those are not getting updated

--- docker/mysql/db/db_upgrade/orgchart/Update_OC_DB_2025021600-2025111000.sql
+++ docker/mysql/db/db_upgrade/orgchart/Update_OC_DB_2025021600-2025111000.sql
@@ -1,12 +1,12 @@
 START TRANSACTION;
 
-ALTER TABLE employee MODIFY COLUMN empUID INT;
-ALTER TABLE relation_position_employee MODIFY COLUMN empUID INT;
-ALTER TABLE relation_group_employee MODIFY COLUMN empUID INT;
-ALTER TABLE relation_employee_backup MODIFY COLUMN empUID INT;
-ALTER TABLE employee_privileges MODIFY COLUMN empUID INT;
-ALTER TABLE employee_data_history MODIFY COLUMN empUID INT;
-ALTER TABLE employee_data MODIFY COLUMN empUID INT;
+ALTER TABLE employee MODIFY COLUMN empUID INT unsigned AUTO_INCREMENT;
+ALTER TABLE relation_position_employee MODIFY COLUMN empUID INT unsigned;
+ALTER TABLE relation_group_employee MODIFY COLUMN empUID INT unsigned;
+ALTER TABLE relation_employee_backup MODIFY COLUMN empUID INT unsigned;
+ALTER TABLE employee_privileges MODIFY COLUMN empUID INT unsigned;
+ALTER TABLE employee_data_history MODIFY COLUMN empUID INT unsigned;
+ALTER TABLE employee_data MODIFY COLUMN empUID INT unsigned;
 
 UPDATE `settings` SET `data` = '2025111000' WHERE `settings`.`setting` = 'dbversion';
 
@@ -16,13 +16,13 @@ COMMIT;
 /**** Revert DB *****
 START TRANSACTION;
 
-ALTER TABLE employee MODIFY COLUMN empUID mediumint;
-ALTER TABLE relation_position_employee MODIFY COLUMN empUID mediumint;
-ALTER TABLE relation_group_employee MODIFY COLUMN empUID mediumint;
-ALTER TABLE relation_employee_backup MODIFY COLUMN empUID mediumint;
-ALTER TABLE employee_privileges MODIFY COLUMN empUID mediumint;
-ALTER TABLE employee_data_history MODIFY COLUMN empUID mediumint;
-ALTER TABLE employee_data MODIFY COLUMN empUID mediumint;
+ALTER TABLE employee MODIFY COLUMN empUID mediumint unsigned AUTO_INCREMENT;
+ALTER TABLE relation_position_employee MODIFY COLUMN empUID mediumint unsigned;
+ALTER TABLE relation_group_employee MODIFY COLUMN empUID mediumint unsigned;
+ALTER TABLE relation_employee_backup MODIFY COLUMN empUID mediumint unsigned;
+ALTER TABLE employee_privileges MODIFY COLUMN empUID mediumint unsigned;
+ALTER TABLE employee_data_history MODIFY COLUMN empUID mediumint unsigned;
+ALTER TABLE employee_data MODIFY COLUMN empUID mediumint unsigned;
 
 UPDATE `settings` SET `data` = '2025021600' WHERE `settings`.`setting` = 'dbversion';
 
